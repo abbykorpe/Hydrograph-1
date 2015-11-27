9e25214965e1b23aa84f9f483eeb71714d0d511c
mahendrakumarp <mahendrakumarp@GEBHSTC7.bitwiseglobal.net>
2015-11-27 15:44:05 +0530

    Changes related to get System env. information in eclipse console and as well as log file.

--- com.bitwise.app.perspective/src/main/java/com/bitwise/app/perspective/Activator.java
+++ com.bitwise.app.perspective/src/main/java/com/bitwise/app/perspective/Activator.java
@@ -1,8 +1,14 @@
 package com.bitwise.app.perspective;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+
+import com.bitwise.app.common.util.LogFactory;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -13,10 +19,11 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.bitwise.app.perspective"; //$NON-NLS-1$
+	private Logger logger = LogFactory.INSTANCE.getLogger(Activator.class);
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -25,16 +32,58 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		logger.info("----System Properties----");
+		logger.info("Operating System : {}", System.getProperty("os.name"));
+		logger.info("JVM : {}", System.getProperty("java.vm.name"));
+		logger.info("java specification version : {}",
+				System.getProperty("java.specification.version"));
+		logger.info("Java Version : {}", System.getProperty("java.version"));
+		logger.info("Osgi OS : {}", System.getProperty("osgi.os"));
+		logger.info("Operating System Version : {}",
+				System.getProperty("os.version"));
+		logger.info("Operating System Architecture : {}",
+				System.getProperty("os.arch"));
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		logger.debug("Screen Width : {}", Double.toString(width));
+		double height = screenSize.getHeight();
+		logger.debug("Screen Height : {}", Double.toString(height));
+
+		int resolution = Toolkit.getDefaultToolkit().getScreenResolution();
+		logger.debug("Screen Resolution :{}", Integer.toString(resolution)
+				.concat(" dpi"));
+
+		Runtime runtime = Runtime.getRuntime();
+
+		long maxMemory = runtime.maxMemory();
+		logger.debug("Max Memory: {}", Long.toString(maxMemory / 1024));
+		long allocatedMemory = runtime.totalMemory();
+		logger.debug("Allocated Memory:  {}",
+				Long.toString(allocatedMemory / 1024));
+		long freeMemory = runtime.freeMemory();
+		logger.debug("Free Memory: {}", Long.toString(freeMemory / 1024));
+		logger.debug("Total free memory: {}", Long
+				.toString((freeMemory + (maxMemory - allocatedMemory)) / 1024));
+		long used = Runtime.getRuntime().totalMemory()
+				- Runtime.getRuntime().freeMemory();
+		logger.debug("Used Menory : {}", Long.toString(used));
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -43,7 +92,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -51,10 +100,11 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
