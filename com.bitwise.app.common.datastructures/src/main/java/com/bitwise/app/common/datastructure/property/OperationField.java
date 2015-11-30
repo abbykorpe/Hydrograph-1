73d9eff41ba9860814fc597cec7c81c0a7b15af2
rakesh kanchalwar <rakesh.kanchalwar@bitwiseglobal.com>
2015-11-30 16:58:12 +0530

    Changes for 1.SonarQube Blocker defect 2.Property window missing tool-tip

--- com.bitwise.app.common.datastructures/src/main/java/com/bitwise/app/common/datastructure/property/OperationField.java
+++ com.bitwise.app.common.datastructures/src/main/java/com/bitwise/app/common/datastructure/property/OperationField.java
@@ -14,6 +14,14 @@ public class OperationField extends PropertyField{
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -27,7 +35,7 @@ public class OperationField extends PropertyField{
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
-		return true; 
+		return true;
 	}
 
 }
