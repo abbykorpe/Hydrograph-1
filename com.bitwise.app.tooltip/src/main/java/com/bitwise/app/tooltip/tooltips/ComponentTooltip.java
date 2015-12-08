3428035d61af8b6218aa2286bd8e49cff9877625
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2015-12-08 14:20:26 +0530

    ComponentTooltip class refactored

--- com.bitwise.app.tooltip/src/main/java/com/bitwise/app/tooltip/tooltips/ComponentTooltip.java
+++ com.bitwise.app.tooltip/src/main/java/com/bitwise/app/tooltip/tooltips/ComponentTooltip.java
@@ -57,6 +57,7 @@ public class ComponentTooltip extends AbstractInformationControl implements IInf
 		super(parent, toolBarManager);
 		this.toolBarManager= getToolBarManager();
 		this.componentToolTipInformation = propertyToolTipInformation;		
+		logger.debug("ComponentTooltip.ComponentTooltip: Creating tooltip with toolbar: " + this.toolBarManager + " , " + this.componentToolTipInformation.toString());
 		create();
 	}
 	
@@ -71,6 +72,7 @@ public class ComponentTooltip extends AbstractInformationControl implements IInf
 	public ComponentTooltip(Shell parent, String status,Map<String,PropertyToolTipInformation> propertyToolTipInformation) {
 		super(parent, status);
 		this.componentToolTipInformation = propertyToolTipInformation;
+		logger.debug("ComponentTooltip.ComponentTooltip: Creating tooltip with statusbar: " + status + " , " + this.componentToolTipInformation.toString());
 		create();
 	}
 	
@@ -82,133 +84,321 @@ public class ComponentTooltip extends AbstractInformationControl implements IInf
 	 */
 	public boolean hasToolBarManager(){
 		if(toolBarManager != null ){
+			logger.debug("ComponentTooltip.hasToolBarManager(): true");
 			return true;
 		}else{
+			logger.debug("ComponentTooltip.hasToolBarManager(): false");
 			return false;
-		}
+		}		
 	}
-	
+		
 	@Override
 	protected void createContent(Composite parent) {
-	
 		ScrolledComposite scrolledComposite = createScrolledComposite(parent);
 		
-		final Composite container = addComposite(scrolledComposite);
-		
-		for(String property: componentToolTipInformation.keySet()){
-			PropertyToolTipInformation propertyInfo = componentToolTipInformation.get(property);
-			if(propertyInfo.isShowAsTooltip()){
-				if(propertyInfo.getPropertyValue()!=null && propertyInfo.getPropertyName().equalsIgnoreCase("OPERATION_CLASS")){							
-					Link link = new Link(container, SWT.NONE);
-					String propertyName = propertyInfo.getPropertyName();
-					String propertyNameCapitalized = WordUtils.capitalize(propertyName.toLowerCase(), '_').replace("_", " ");
-					
-					String tempText= propertyNameCapitalized + " : <a>" + propertyInfo.getPropertyValue().toString() + "</a>";
-					final String filePath = propertyInfo.getPropertyValue().toString();
-					link.setText(tempText);
-					link.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-					
-					link.addSelectionListener(new SelectionAdapter() {
-						@Override
-						public void widgetSelected(SelectionEvent e) {
-							// TODO Auto-generated method stub
-							super.widgetSelected(e);
-							boolean flag = FilterOperationClassUtility.openFileEditor(filePath);
-							if (!flag) {
-								WidgetUtility.errorMessage("File Not Found"); 
-							} else {
-								setVisible(false);
-							}
-						}
-					});
-					showErrors(propertyInfo, link);
-				}else{
-					addPropertyToToolTip(parent, container, propertyInfo);
-				}
-			}
-		}
-		
-		container.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-		
-		container.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseUp(MouseEvent e) {
-				// TODO Auto-generated method stub
-				container.setFocus();
-			}
-			
-			@Override
-			public void mouseDown(MouseEvent e) {
-				// TODO Auto-generated method stub
+		final Composite container = createToolTipContainer(parent,
+				scrolledComposite);
 				
-			}
-			
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		addToolTipContents(container);
 		
-		//container.setFocus();
-		
+		refreshScrolledComposite(scrolledComposite, container);
+	}
+
+	private void refreshScrolledComposite(ScrolledComposite scrolledComposite,
+			final Composite container) {
 		scrolledComposite.setContent(container);
 		scrolledComposite.setMinSize(container.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 	}
 
-	private void addPropertyToToolTip(Composite parent,
-			final Composite container, PropertyToolTipInformation propertyInfo) {
-		Label lblTextProperty = new Label(container, SWT.NONE);
+	/**
+	 * builds tooltip by adding property information on tooltip container
+	 * 
+	 * @param container
+	 */
+	private void addToolTipContents(final Composite container) {
+		for(String property: componentToolTipInformation.keySet()){
+			PropertyToolTipInformation propertyInfo = componentToolTipInformation.get(property);
+			if(propertyInfo.isShowAsTooltip()){
+				addPropertyInformationToToolTip(container, propertyInfo);
+			}
+		}
+	}
+
+	/**
+	 * add Property Information To ToolTip
+	 * @param container
+	 * @param propertyInfo
+	 */
+	private void addPropertyInformationToToolTip(final Composite container,
+			PropertyToolTipInformation propertyInfo) {
+		if(propertyInfo.getPropertyValue()!=null && propertyInfo.getPropertyName().equalsIgnoreCase("OPERATION_CLASS")){	
+			logger.debug("ComponentTooltip.addToolTipContents(): Its Opeartion class");
+			addOperationClassPropertyToToolTip(container, propertyInfo);
+		}else{
+			logger.debug("ComponentTooltip.addToolTipContents(): Its other property or with Opeartion class=null");
+			addPropertyToToolTip(container, propertyInfo);
+		}
+	}
+
+	/**
+	 * add operation class property to tooltip container
+	 * 
+	 * @param container
+	 * @param propertyInfo
+	 */
+	private void addOperationClassPropertyToToolTip(final Composite container,
+			PropertyToolTipInformation propertyInfo) {
+		String propertyNameCapitalized = getCapitalizedName(propertyInfo);
+		
+		final String filePath = propertyInfo.getPropertyValue().toString();
+		logger.debug("ComponentTooltip.addOperationClassPropertyToToolTip(): Opeartion class filePath=" + filePath);
+		
+		Link link = createOperationClassLink(container, propertyInfo,
+				propertyNameCapitalized, filePath);
+				
+		showErrors(propertyInfo, link);
+	}
+
+	/**
+	 * Create operation class link
+	 * 
+	 * @param container
+	 * @param propertyInfo
+	 * @param propertyNameCapitalized
+	 * @param filePath
+	 * @return
+	 */
+	private Link createOperationClassLink(final Composite container,
+			PropertyToolTipInformation propertyInfo,
+			String propertyNameCapitalized, final String filePath) {
+		Link link = new Link(container, SWT.NONE);
+		String tempText= propertyNameCapitalized + " : <a>" + propertyInfo.getPropertyValue().toString() + "</a>";		
+		link.setText(tempText);
+		link.setBackground(container.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
+		
+		addListenerToOpenOpeartionClassFile(filePath, link);
+		
+		logger.debug("ComponentTooltip.createOperationClassLink(): created opeartion class link=" + link);
+		
+		return link;
+	}
+
+	/**
+	 * Add listener to open operation class file
+	 * 
+	 * @param filePath
+	 * @param link
+	 */
+	private void addListenerToOpenOpeartionClassFile(final String filePath,
+			Link link) {
+		link.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				logger.debug("ComponentTooltip.widgetSelected(): Link clicked");
+				super.widgetSelected(e);
+				boolean flag = FilterOperationClassUtility.openFileEditor(filePath);
+				if (!flag) {
+					logger.debug("ComponentTooltip.widgetSelected(): Link clicked - error - File " + filePath + " Not Found");
+					WidgetUtility.errorMessage("File Not Found"); 
+				} else {
+					logger.debug("ComponentTooltip.widgetSelected(): Link clicked - hiding tooltip");
+					setVisible(false);
+				}
+			}
+		});
+		
+		logger.debug("ComponentTooltip.addListenerToOpenOpeartionClassFile(): added opeartion class link listener");
+	}
+
+	/**
+	 * get Capitalized property name
+	 * @param propertyInfo
+	 * @return
+	 */
+	private String getCapitalizedName(PropertyToolTipInformation propertyInfo) {
+		logger.debug("ComponentTooltip.getCapitalizedName(): propertyInfo: " + propertyInfo.toString());
+		
 		String propertyName = propertyInfo.getPropertyName();
 		String propertyNameCapitalized = WordUtils.capitalize(propertyName.toLowerCase(), '_').replace("_", " ");
-		if(propertyInfo.getPropertyValue() != null){
-			if(propertyInfo.getTooltipDataType().equalsIgnoreCase("LIST")){
-				lblTextProperty.setText(propertyNameCapitalized + " : " + propertyInfo.getPropertyValue());
-			}else{
-				lblTextProperty.setText(propertyNameCapitalized + " : " + propertyInfo.getPropertyValue().toString());
-			}
 				
-		}else{
-			lblTextProperty.setText(propertyNameCapitalized + " : ");
-		}
-		lblTextProperty.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-		lblTextProperty.addListener(SWT.MouseUp, getMouseClickListener(container));						
+		logger.debug("ComponentTooltip.getCapitalizedName(): propertyNameCapitalized: " + propertyNameCapitalized);
+		
+		return propertyNameCapitalized;
+	}
+
+	/**
+	 * Creates container for tooltip
+	 * @param parent
+	 * @param scrolledComposite
+	 * @return
+	 */
+	private Composite createToolTipContainer(Composite parent,
+			ScrolledComposite scrolledComposite) {
+		final Composite container = addComposite(scrolledComposite);
+		container.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
+		addToolTipContainerFocusListener(container);
+		
+		logger.debug("ComponentTooltip.createToolTipContainer() - created Tooltip container " + container);
+		return container;
+	}
+	
+	/**
+	 * adds listener to focus container when clicked on container 
+	 * @param container
+	 */
+	private void addToolTipContainerFocusListener(final Composite container) {
+		container.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				container.setFocus();
+				logger.debug("ComponentTooltip.addToolTipContainerFocusListener() - mouseUp - container focused");
+			}
+			
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// Do nothing				
+			}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// Do nothing				
+			}
+		});
+		
+		logger.debug("ComponentTooltip.addToolTipContainerFocusListener() - added container focus listener");
+	}
+
+	/**
+	 * Add property to tooltip
+	 * 
+	 * @param container
+	 * @param propertyInfo
+	 */
+	private void addPropertyToToolTip(final Composite container, PropertyToolTipInformation propertyInfo) {
+		Label lblTextProperty = new Label(container, SWT.NONE);
+		String propertyNameCapitalized = getCapitalizedName(propertyInfo);
+		
+		logger.debug("ComponentTooltip.addPropertyToToolTip() - propertyInfo=" + propertyInfo.toString());
+		
+		addText(propertyInfo, lblTextProperty, propertyNameCapitalized);
+		
+		lblTextProperty.setBackground(container.getDisplay().getSystemColor(SWT.COLOR_INFO_BACKGROUND));
+		lblTextProperty.addListener(SWT.MouseUp, getMouseClickListener(container));
+		
 		showErrors(propertyInfo, lblTextProperty);
 	}
 
+	/**
+	 * Add text to tooltip
+	 * @param propertyInfo
+	 * @param lblTextProperty
+	 * @param propertyNameCapitalized
+	 */
+	private void addText(PropertyToolTipInformation propertyInfo,
+			Label lblTextProperty, String propertyNameCapitalized) {
+		if(propertyInfo.getPropertyValue() != null){
+			addPropertyNameValueText(propertyInfo, lblTextProperty,
+					propertyNameCapitalized);
+		}else{
+			addPropertyWithNoValue(lblTextProperty, propertyNameCapitalized);
+		}
+	}
+
+	/**
+	 * Add text with no value
+	 * @param lblTextProperty
+	 * @param propertyNameCapitalized
+	 */
+	private void addPropertyWithNoValue(Label lblTextProperty,
+			String propertyNameCapitalized) {
+		logger.debug("ComponentTooltip.addPropertyToToolTip() - property with no value");
+		lblTextProperty.setText(propertyNameCapitalized + " : ");
+	}
+
+	/**
+	 * Add text with name:value
+	 * 
+	 * @param propertyInfo
+	 * @param lblTextProperty
+	 * @param propertyNameCapitalized
+	 */
+	private void addPropertyNameValueText(
+			PropertyToolTipInformation propertyInfo, Label lblTextProperty,
+			String propertyNameCapitalized) {
+		if(propertyInfo.getTooltipDataType().equalsIgnoreCase("LIST")){
+			logger.debug("ComponentTooltip.addPropertyToToolTip() - property type is LIST=");
+			lblTextProperty.setText(propertyNameCapitalized + " : " + propertyInfo.getPropertyValue());
+		}else{
+			logger.debug("ComponentTooltip.addPropertyToToolTip() - property type is Text/Map=");
+			lblTextProperty.setText(propertyNameCapitalized + " : " + propertyInfo.getPropertyValue().toString());
+		}
+	}
+
+	/**
+	 *  add scrolled composite to make tooltip scrollable.
+	 * @param scrolledComposite
+	 * @return
+	 */
 	private Composite addComposite(ScrolledComposite scrolledComposite) {
 		final Composite container = new Composite(scrolledComposite, SWT.NONE);
 		container.setLayout(new GridLayout(1, true));
 		return container;
 	}
 
+	/**
+	 * Create scrolled composite
+	 * @param parent
+	 * @return
+	 */
 	private ScrolledComposite createScrolledComposite(Composite parent) {
 		ScrolledComposite scrolledComposite = new ScrolledComposite(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
+		
+		logger.debug("ComponentTooltip.createScrolledComposite() - created scrolled composite " + scrolledComposite);
+		
 		return scrolledComposite;
 	}
 
+	/**
+	 * Show errors balloon in case property is having errors.
+	 * 
+	 * @param propertyInfo
+	 * @param lblLinkProperty
+	 */
 	private void showErrors(PropertyToolTipInformation propertyInfo,
 			Control lblLinkProperty) {
 		if(propertyInfo.getErrorMessage()!=null){
+			logger.debug("ComponentTooltip.showErrors() - Showing error balloon on property " + propertyInfo.getPropertyName() + 
+					", Error: " + propertyInfo.getErrorMessage());
 			ControlDecoration lblDecorator = WidgetUtility.addDecorator(lblLinkProperty, propertyInfo.getErrorMessage());
 			lblDecorator.show();
 		}
 	}
 
+	/**
+	 * 
+	 * Mouse click listener to set focus on tooltip container
+	 * 
+	 * @param container
+	 * @return
+	 */
 	private Listener getMouseClickListener(final Composite container) {
 		return new Listener() {
 			
 			@Override
 			public void handleEvent(Event event) {
+				logger.debug("ComponentTooltip.getMouseClickListener() - setting foucs to container");
 				container.setFocus();
 			}
 		};
 	}
 
-	
+	/**
+	 * returns tooltip container object
+	 * @return Composite - tooltip composite
+	 */
 	public Composite getTooltipContainer(){
 		return tooltipContainer;
 	}
