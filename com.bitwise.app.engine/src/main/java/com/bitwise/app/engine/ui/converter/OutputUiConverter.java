72d55b18e03c910d4b7a829381a4ea7ca3712aad
Nitin Gupta <nitin.gupta@bitwiseglobal.com>
2015-12-11 20:48:53 +0530

    target to ui changes

--- com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/ui/converter/OutputUiConverter.java
+++ com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/ui/converter/OutputUiConverter.java
@@ -1,29 +1,34 @@
 package com.bitwise.app.engine.ui.converter;
 
+import org.slf4j.Logger;
+
+import com.bitwise.app.common.util.LogFactory;
 import com.bitwise.app.engine.constants.PropertyNameConstants;
-import com.bitwise.app.engine.ui.constants.UIComponentsPort;
 import com.bitwise.app.engine.ui.repository.UIComponentRepo;
-import com.bitwiseglobal.graph.commontypes.TypeBaseInSocket;
 import com.bitwiseglobal.graph.commontypes.TypeOutputComponent;
 import com.bitwiseglobal.graph.commontypes.TypeOutputInSocket;
 
 public abstract class OutputUiConverter extends UiConverter {
-
+	private static final Logger LOGGER = LogFactory.INSTANCE.getLogger(OutputUiConverter.class);
+	
 	@Override
 	public void prepareUIXML() {
-
 		super.prepareUIXML();
+		LOGGER.debug("Fetching common properties for -{}",COMPONENT_NAME);
 		getInPort((TypeOutputComponent) typeBaseComponent);
 		propertyMap.put(PropertyNameConstants.RUNTIME_PROPERTIES.value(),getRuntimeProperties());
 	}
 
 	protected void getInPort(TypeOutputComponent typeOutputComponent) {
+		LOGGER.debug("Generating Input Ports for -{}",COMPONENT_NAME);
 		int portCounter = 1;
 		if (typeOutputComponent.getInSocket() != null) {
 			for (TypeOutputInSocket inSocket : typeOutputComponent.getInSocket()) {
 				if(inSocket.getSchema()!=null)
-					getSchema(inSocket);						
-				uiComponent.engageInputPort((UIComponentsPort.getPortType(inSocket.getType())) + portCounter);
+				{
+					propertyMap.put(PropertyNameConstants.SCHEMA.value(),getSchema(inSocket));
+				}
+				uiComponent.engageInputPort(inSocket.getType() + portCounter);
 				UIComponentRepo.INSTANCE.getComponentLinkList().add(
 						new LinkingData(inSocket.getFromComponentId(),
 								typeOutputComponent.getId(),
@@ -34,7 +39,6 @@ public abstract class OutputUiConverter extends UiConverter {
 			}
 		}
 	}
-	
 	
 	protected abstract Object getSchema(TypeOutputInSocket inSocket) ;
 }
--- com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/ui/converter/UiConverter.java
+++ com.bitwise.app.engine/src/main/java/com/bitwise/app/engine/ui/converter/UiConverter.java
@@ -9,18 +9,16 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 
 import com.bitwise.app.common.util.LogFactory;
-import com.bitwise.app.engine.converter.Converter;
 import com.bitwise.app.engine.ui.repository.ParameterData;
 import com.bitwise.app.engine.ui.repository.UIComponentRepo;
 import com.bitwise.app.graph.model.Component;
 import com.bitwise.app.graph.model.Container;
-import com.bitwise.app.graph.model.Link;
 import com.bitwiseglobal.graph.commontypes.BooleanValueType;
 import com.bitwiseglobal.graph.commontypes.TypeBaseComponent;
 
 public abstract class UiConverter {
 	
-	private static final Logger LOGGER = LogFactory.INSTANCE.getLogger(Converter.class);
+	private static final Logger LOGGER = LogFactory.INSTANCE.getLogger(UiConverter.class);
 	protected Container container;
 	protected Component uiComponent = null;
 	protected TypeBaseComponent typeBaseComponent=null;
@@ -28,27 +26,26 @@ public abstract class UiConverter {
 	protected static final String PHASE = "phase";
 	protected static final String NAME = "name";
 	protected String COMPONENT_NAME;
-	protected Link link=null;
+	
 	
 	
 	public void prepareUIXML() {
 		COMPONENT_NAME=typeBaseComponent.getId();
+		LOGGER.debug("Preparing basic properties for component {}",COMPONENT_NAME);		
 		propertyMap.put(NAME,COMPONENT_NAME);
 		propertyMap.put(PHASE,typeBaseComponent.getPhase().toString());
 		uiComponent.setComponentLabel(COMPONENT_NAME);
 		uiComponent.setParent(container);
-		
 		UIComponentRepo.INSTANCE.getComponentUiFactory().put(COMPONENT_NAME,uiComponent);
 	}
 
 	public Component getComponent() {
-		
 		return uiComponent;
 	}
 	public String convertBooleanVlaue(BooleanValueType value, String propertyName)
-	{
+	{  
+		LOGGER.debug("Converting Boolean to String - {}",propertyName);		
 		Object stringValue=null;
-
 		if(value!=null && value.isValue()!=null && value.isValue().toString().equalsIgnoreCase("true"))
 		{
 			return "True";
@@ -58,13 +55,12 @@ public abstract class UiConverter {
 			{
 				return (String)stringValue;
 			}
-//			return stringValue;
 		}
 		
 	}
 	
 	protected Object getValue(String propertyName) {
-	
+		LOGGER.debug("Getting Parameter for - {}",propertyName);	
 		List<ParameterData> parameterList=UIComponentRepo.INSTANCE.getParammeterFactory().get(COMPONENT_NAME);
 		if(parameterList!=null)
 		{
@@ -82,18 +78,17 @@ public abstract class UiConverter {
 	
 	protected boolean isParameter(String parameter) {
 		if(parameter!=null){
-		String Regex="[\\@]{1}[\\{]{1}[\\w]*[\\}]{1}";
-		Matcher matchs = Pattern.compile(Regex).matcher(parameter);
-		if(matchs.matches())
-		{
-			return true;
-		}
+			String Regex="[\\@]{1}[\\{]{1}[\\w]*[\\}]{1}";
+			Matcher matchs = Pattern.compile(Regex).matcher(parameter);
+			if(matchs.matches())
+			{
+				return true;
+			}
 		}
 		return false;
 	}
 	
 	protected abstract Map<String,String> getRuntimeProperties();
-	
 	
 	protected String getStringValue(String value)
 	{ String emptyString="";
@@ -102,5 +97,5 @@ public abstract class UiConverter {
 		else
 			return value;
 	}
-	
+
 }
