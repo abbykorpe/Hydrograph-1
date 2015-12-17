ee7892c197f666e6b4abed2739fe217d846d2c09
kanchank <kanchank@GEBHSTC1.bitwiseglobal.net>
2015-12-17 17:06:15 +0530

    Added menu for 'run configuration settings' and its dialog box.

---
+++ com.bitwise.app.menus/src/com/bitwise/app/menus/handlers/RunConfigHandler.java
@@ -0,0 +1,31 @@
+package com.bitwise.app.menus.handlers;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.swt.widgets.Display;
+
+import com.bitwise.app.propertywindow.runconfig.RunConfigDialog;
+
+/**
+ * The Class Component.
+ * 
+ * @author Bitwise
+ */
+
+public class RunConfigHandler extends AbstractHandler implements IHandler {
+
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		 
+		 RunConfigDialog runConfig = new RunConfigDialog(Display.getDefault().getActiveShell());
+			try{
+				runConfig.open();
+			}catch(Exception e){
+				// Do nothing
+			}
+		return null;
+	}
+
+}
