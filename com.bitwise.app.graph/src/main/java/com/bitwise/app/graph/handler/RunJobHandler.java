3630a744e96d496339f87288f983b4af9268b226
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-01-12 12:30:19 +0530

    Below feedback item fix The project explorer must refresh the project folders on addition or deletion of folders. Currently user has to manually refresh the project to view newly added folders

--- com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/handler/RunJobHandler.java
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/handler/RunJobHandler.java
@@ -5,23 +5,28 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 
 import com.bitwise.app.common.interfaces.parametergrid.DefaultGEFCanvas;
@@ -103,6 +108,7 @@ public class RunJobHandler extends AbstractHandler {
 		joblogger.logSystemInformation();
 		try {
 			IEditorPart iEditorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+			final String projectName=((IFileEditorInput)iEditorPart.getEditorInput()).getFile().getProject().getName();
 			String XML_PATH = "";
 			if (iEditorPart != null) {
 				XML_PATH = iEditorPart.getEditorInput().getToolTipText().replace(Messages.JOBEXTENSION, Messages.XMLEXTENSION);
@@ -137,6 +143,12 @@ public class RunJobHandler extends AbstractHandler {
 							joblogger.logJobEndInfo();
 							joblogger.close();
 							setBaseEnabled(true);
+							IProject iProject=ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+							try {
+								iProject.refreshLocal(IResource.DEPTH_INFINITE, null);
+							} catch (CoreException e) {
+								logger.error("Error while refreshing the project",e);
+							}
 						}
 						
 					}).start();
