d1fdf79f2aca6fc3139db70ffc48a1d2885256f7
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-01-18 10:52:16 +0530

    integrate input delimited and input fixed width html files with palatte help

--- com.bitwise.app.tooltip/src/main/java/com/bitwise/app/component/help/IFDelimited.java
+++ com.bitwise.app.tooltip/src/main/java/com/bitwise/app/component/help/IFDelimited.java
@@ -7,7 +7,7 @@ public class IFDelimited implements ComponentHelp {
 	@Override
 	public void HelpContentofComponent() {
 		PlatformUI.getWorkbench().getHelpSystem().displayHelpResource(
-				   "/com.bitwise.app.help/html/Components/InputFileDelimited.html");
+				   "/com.bitwise.app.help/html/Components/Input_File_Delimited.html");
 	}
 
 }
