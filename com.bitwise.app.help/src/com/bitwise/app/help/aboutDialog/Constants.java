7ee45e7988c36ae75947aa6acf64d6258e1f2d67
Vibhor Tyagi <vibhor.tyagi@bitwiseglobal.com>
2016-02-03 17:22:14 +0530

    help content related changes

--- com.bitwise.app.help/src/com/bitwise/app/help/aboutDialog/Constants.java
+++ com.bitwise.app.help/src/com/bitwise/app/help/aboutDialog/Constants.java
@@ -7,8 +7,7 @@ package com.bitwise.app.help.aboutDialog;
  */
 public class Constants {
 	
-	public static final String ABOUT_TEXT="Accelero for ETL Developers\n\n Version: Accelero Service Release 1\n\n (c) Copyright Accelero contributors.  All rights reserved.\n" +
-			"Visit http://Accelero.org/";
+	public static final String ABOUT_TEXT="Accelero for ETL Developers\n\n Version: Accelero Service Release 1 \t\t";
 	public static final String ABOUT_DIALOG_IMAGE_PATH="icons/alt_about.gif";
 	public static final String ABOUT_DIALOG_FEATURE_IMAGE_PATH="icons/app_icon.png";
 	public static final String ABOUT_DIALOG_IMAGE_BUNDLE_NAME="com.bitwise.app.perspective";
