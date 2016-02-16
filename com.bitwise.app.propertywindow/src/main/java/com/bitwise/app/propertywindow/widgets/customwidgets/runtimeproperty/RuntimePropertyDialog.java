eb1a4bff36622f3ace42a27ec984e22fe6d570a6
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2016-02-16 15:23:47 +0530

    Runtime properties - removed discrepancies in OK and Cancel button

---
+++ com.bitwise.app.propertywindow/src/main/java/com/bitwise/app/propertywindow/widgets/customwidgets/runtimeproperty/RuntimePropertyDialog.java
@@ -0,0 +1,554 @@
+package com.bitwise.app.propertywindow.widgets.customwidgets.runtimeproperty;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnViewerEditor;
+import org.eclipse.jface.viewers.ColumnViewerEditorActivationStrategy;
+import org.eclipse.jface.viewers.ICellEditorValidator;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerEditor;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.forms.widgets.ColumnLayout;
+import org.eclipse.ui.forms.widgets.ColumnLayoutData;
+
+import com.bitwise.app.common.util.XMLConfigUtil;
+import com.bitwise.app.propertywindow.messages.Messages;
+import com.bitwise.app.propertywindow.propertydialog.PropertyDialogButtonBar;
+
+public class RuntimePropertyDialog extends Dialog {
+
+	private static final String ERROR = "ERROR";
+	private static final String PROPERTY_NAME = "PROPERTY_NAME";
+	private static final String PROPERTY_VALUE = "PROPERTY_VALUE";
+
+	private static final String REGEX_PPATTERN = "[\\@]{1}[\\{]{1}[\\w]*[\\}]{1}||[\\w]*";
+	private static final String[] PROPS = { PROPERTY_NAME, PROPERTY_VALUE };
+
+	private static final String ICONS_DOWN_PNG = "/icons/down.png";
+	private static final String ICONS_UP_PNG = "/icons/up.png";
+	private static final String ICONS_DELETE_PNG = "/icons/delete.png";
+	private static final String ICONS_ADD_PNG = "/icons/add.png";
+	private static final String ICONS_PROPERTY_WINDOW_ICON_PNG = "/icons/property_window_icon.png";
+
+	private String PROPERTY_EXISTS_ERROR = Messages.RuntimePropertAlreadyExists;
+	private String PROPERTY_NAME_BLANK_ERROR = Messages.EmptyNameNotification;
+	private String PROPERTY_VALUE_BLANK_ERROR = Messages.EmptyValueNotification;
+	private TableViewer tableViewer;
+
+	private List<RuntimeProperties> propertyList;
+	private Map<String, String> runtimePropertyMap;
+
+	private Table table;
+	private Label lblPropertyError;
+
+
+	/**
+	 * Create the dialog.
+	 * 
+	 * @param parentShell
+	 * @param propertyDialogButtonBar
+	 */
+	public RuntimePropertyDialog(Shell parentShell) {
+		super(parentShell);
+
+		propertyList = new LinkedList<RuntimeProperties>();
+		runtimePropertyMap = new LinkedHashMap<String, String>();
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		ColumnLayout cl_container = new ColumnLayout();
+		cl_container.verticalSpacing = 0;
+		cl_container.maxNumColumns = 1;
+		container.setLayout(cl_container);
+
+		addSeperator(container);
+		addButtonPanel(container);
+
+		Composite composite_2 = new Composite(container, SWT.NONE);
+		composite_2.setLayout(new GridLayout(1, false));
+		ColumnLayoutData cld_composite_2 = new ColumnLayoutData();
+		cld_composite_2.heightHint = 336;
+		composite_2.setLayoutData(cld_composite_2);
+
+		createTable(composite_2);
+
+		addErrorLabel(container);
+
+		return container;
+	}
+
+	private void addSeperator(Composite container) {
+		Composite composite = new Composite(container, SWT.NONE);
+		ColumnLayout cl_composite = new ColumnLayout();
+		cl_composite.maxNumColumns = 1;
+		composite.setLayout(cl_composite);
+		ColumnLayoutData cld_composite = new ColumnLayoutData();
+		cld_composite.heightHint = 28;
+		composite.setLayoutData(cld_composite);
+
+		Label lblTestlabel = new Label(composite, SWT.NONE);
+		lblTestlabel.setText(Messages.RUNTIME_HEADER);
+
+		new Label(composite, SWT.SEPARATOR | SWT.HORIZONTAL);
+	}
+
+	private void addErrorLabel(Composite container) {
+		Composite composite_3 = new Composite(container, SWT.NONE);
+		composite_3.setLayout(new ColumnLayout());
+		ColumnLayoutData cld_composite_3 = new ColumnLayoutData();
+		cld_composite_3.heightHint = 25;
+		composite_3.setLayoutData(cld_composite_3);
+
+		lblPropertyError = new Label(composite_3, SWT.NONE);
+		lblPropertyError.setVisible(false);
+		lblPropertyError.setForeground(new Color(Display.getDefault(), 255, 0, 0));
+	}
+
+	public Map<String, String> getRuntimeProperties() {
+		return runtimePropertyMap;
+	}
+
+	private void addButtonPanel(Composite container) {
+		Composite composite_1 = new Composite(container, SWT.NONE);
+		composite_1.setLayout(new GridLayout(4, false));
+		ColumnLayoutData cld_composite_1 = new ColumnLayoutData();
+		cld_composite_1.horizontalAlignment = ColumnLayoutData.RIGHT;
+		cld_composite_1.heightHint = 28;
+		composite_1.setLayoutData(cld_composite_1);
+
+		Label addButton = new Label(composite_1, SWT.NONE);
+		addButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		addButton.setImage(new Image(null, XMLConfigUtil.INSTANCE.CONFIG_FILES_PATH + "/icons/add.png"));
+
+		attachAddButtonListern(addButton);
+
+		Label deleteButton = new Label(composite_1, SWT.NONE);
+		deleteButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		deleteButton.setImage(new Image(null, XMLConfigUtil.INSTANCE.CONFIG_FILES_PATH + "/icons/delete.png"));
+		attachDeleteButtonListener(deleteButton);
+
+		Label upButton = new Label(composite_1, SWT.NONE);
+		upButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		upButton.setImage(new Image(null, XMLConfigUtil.INSTANCE.CONFIG_FILES_PATH + "/icons/up.png"));
+		attachUpButtonListener(upButton);
+
+		Label downButton = new Label(composite_1, SWT.NONE);
+		downButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		downButton.setImage(new Image(null, XMLConfigUtil.INSTANCE.CONFIG_FILES_PATH + "/icons/down.png"));
+		attachDownButtonListerner(downButton);
+	}
+
+	private void attachDownButtonListerner(Label downButton) {
+		downButton.addMouseListener(new MouseListener() {
+			int index1 = 0, index2 = 0;
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				index1 = table.getSelectionIndex();
+
+				if (index1 < propertyList.size() - 1) {
+					String text = tableViewer.getTable().getItem(index1).getText(0);
+					String text1 = tableViewer.getTable().getItem(index1).getText(1);
+
+					index2 = index1 + 1;
+
+					String data = tableViewer.getTable().getItem(index2).getText(0);
+					String data1 = tableViewer.getTable().getItem(index2).getText(1);
+
+					RuntimeProperties p = new RuntimeProperties();
+					p.setPropertyName(data);
+					p.setPropertyValue(data1);
+					propertyList.set(index1, p);
+					p = new RuntimeProperties();
+					p.setPropertyName(text);
+					p.setPropertyValue(text1);
+					propertyList.set(index2, p);
+					tableViewer.refresh();
+					table.setSelection(index1 + 1);
+				}
+			}
+		});
+
+	}
+
+	private void attachUpButtonListener(Label upButton) {
+		upButton.addMouseListener(new MouseListener() {
+			int index1 = 0, index2 = 0;
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				index1 = table.getSelectionIndex();
+
+				if (index1 > 0) {
+					String text = tableViewer.getTable().getItem(index1).getText(0);
+					String text1 = tableViewer.getTable().getItem(index1).getText(1);
+					index2 = index1 - 1;
+					String data = tableViewer.getTable().getItem(index2).getText(0);
+					String data2 = tableViewer.getTable().getItem(index2).getText(1);
+
+					RuntimeProperties p = new RuntimeProperties();
+					p.setPropertyName(data);
+					p.setPropertyValue(data2);
+					propertyList.set(index1, p);
+					p = new RuntimeProperties();
+					p.setPropertyName(text);
+					p.setPropertyValue(text1);
+					propertyList.set(index2, p);
+					tableViewer.refresh();
+					table.setSelection(index1 - 1);
+				}
+			}
+		});
+
+	}
+
+	private void attachDeleteButtonListener(Label deleteButton) {
+		deleteButton.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// Nothing to do
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// Nothing to do
+			}
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				IStructuredSelection selection = (IStructuredSelection) tableViewer.getSelection();
+				for (Iterator<?> iterator = selection.iterator(); iterator.hasNext();) {
+					Object selectedObject = iterator.next();
+					tableViewer.remove(selectedObject);
+					propertyList.remove(selectedObject);
+
+				}
+			}
+
+		});
+
+	}
+
+	private void attachAddButtonListern(Label addButton) {
+		addButton.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// Nothing to do
+
+			}
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+				table.getParent().setFocus();
+				addNewProperty(tableViewer);
+
+			}
+
+		});
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	@Override
+	protected void okPressed() {
+
+		if (validate()) {
+			runtimePropertyMap.clear();
+			for (RuntimeProperties temp : propertyList) {
+				runtimePropertyMap.put(temp.getPropertyName(), temp.getPropertyValue());
+			}
+			super.okPressed();
+		}
+
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(497, 499);
+	}
+
+	// Add New Property After Validating old properties
+	private void addNewProperty(TableViewer tabViewer) {
+		RuntimeProperties property = new RuntimeProperties();
+		if (propertyList.size() != 0) {
+			if (!validate()) {
+				return;
+			}
+			property.setPropertyName("");
+			property.setPropertyValue("");
+			propertyList.add(property);
+			tabViewer.refresh();
+			tableViewer.editElement(tableViewer.getElementAt(propertyList.size() - 1), 0);
+		} else {
+			property.setPropertyName("");
+			property.setPropertyValue("");
+			propertyList.add(property);
+			tabViewer.refresh();
+			tableViewer.editElement(tableViewer.getElementAt(0), 0);
+		}
+	}
+
+	public void setRuntimeProperties(Map<String, String> runtimePropertyMap) {
+		this.runtimePropertyMap = runtimePropertyMap;
+	}
+
+	// Loads Already Saved Properties..
+	private void loadProperties(TableViewer tv) {
+
+		if (runtimePropertyMap != null && !runtimePropertyMap.isEmpty()) {
+			for (String key : runtimePropertyMap.keySet()) {
+				RuntimeProperties p = new RuntimeProperties();
+				if (validateBeforeLoad(key, runtimePropertyMap.get(key))) {
+					p.setPropertyName(key);
+					p.setPropertyValue(runtimePropertyMap.get(key));
+					propertyList.add(p);
+				}
+			}
+			tv.refresh();
+
+		} //$NON-NLS-1$
+
+	}
+
+	private boolean validateBeforeLoad(String key, String keyValue) {
+
+		if (key.trim().isEmpty() || keyValue.trim().isEmpty()) {
+			return false;
+		}
+		return true;
+
+	}
+
+	// Method for creating Table
+	private void createTable(Composite composite) {
+		tableViewer = new TableViewer(composite, SWT.BORDER | SWT.MULTI | SWT.FULL_SELECTION);
+		table = tableViewer.getTable();
+		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd_table.widthHint = 431;
+		table.setLayoutData(gd_table);
+
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				addNewProperty(tableViewer);
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+				lblPropertyError.setVisible(false);
+			}
+		});
+		tableViewer.getTable().addTraverseListener(new TraverseListener() {
+			@Override
+			public void keyTraversed(TraverseEvent e) {
+				if (e.keyCode == SWT.ARROW_UP) {
+					e.doit = false;
+				} else if (e.keyCode == SWT.ARROW_DOWN) {
+					e.doit = false;
+				} else if (e.keyCode == SWT.TRAVERSE_ARROW_NEXT) {
+					e.doit = false;
+				} else if (e.keyCode == SWT.TRAVERSE_ARROW_PREVIOUS) {
+					e.doit = false;
+				}
+
+			}
+		});
+
+		// table.setBounds(10, 68, 465, 400);
+		tableViewer.setContentProvider(new PropertyContentProvider());
+		tableViewer.setLabelProvider(new PropertyLabelProvider());
+		tableViewer.setInput(propertyList);
+
+		TableColumn column1 = new TableColumn(table, SWT.CENTER);
+		column1.setText(Messages.PROPERTY_NAME);
+		TableColumn column2 = new TableColumn(table, SWT.LEFT_TO_RIGHT);
+		column2.setText(Messages.PROPERTY_VALUE);
+
+		for (int i = 0, n = table.getColumnCount(); i < n; i++) {
+			table.getColumn(i).pack();
+		}
+		column1.setWidth(230);
+		column2.setWidth(236);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+
+		final CellEditor propertyNameeditor = new TextCellEditor(table);
+
+		CellEditor propertyValueeditor = new TextCellEditor(table);
+		CellEditor[] editors = new CellEditor[] { propertyNameeditor, propertyValueeditor };
+
+		propertyNameeditor.setValidator(createNameEditorValidator(PROPERTY_NAME_BLANK_ERROR));
+		propertyValueeditor.setValidator(createValueEditorValidator(PROPERTY_VALUE_BLANK_ERROR));
+
+		tableViewer.setColumnProperties(PROPS);
+		tableViewer.setCellModifier(new RunTimePropertyCellModifier(tableViewer));
+		tableViewer.setCellEditors(editors);
+
+		// enables the tab functionality
+		TableViewerEditor.create(tableViewer, new ColumnViewerEditorActivationStrategy(tableViewer),
+				ColumnViewerEditor.KEYBOARD_ACTIVATION | ColumnViewerEditor.TABBING_HORIZONTAL
+						| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR | ColumnViewerEditor.TABBING_VERTICAL);
+
+		loadProperties(tableViewer);
+
+	}
+
+	/**
+	 * Validate.
+	 * 
+	 * @return true, if successful
+	 */
+	protected boolean validate() {
+		int propertyCounter = 0;
+		for (RuntimeProperties temp : propertyList) {
+			if (!temp.getPropertyName().trim().isEmpty() && !temp.getPropertyValue().trim().isEmpty()) {
+				String Regex = REGEX_PPATTERN;
+				Matcher matchName = Pattern.compile(Regex).matcher(temp.getPropertyName());
+				Matcher matchValue = Pattern.compile(Regex).matcher(temp.getPropertyValue());
+				if (!matchName.matches() || !matchValue.matches()) {
+					table.setSelection(propertyCounter);
+					lblPropertyError.setVisible(true);
+					lblPropertyError.setText(Messages.ALLOWED_CHARACTERS);
+					return false;
+				}
+
+			} else {
+				table.setSelection(propertyCounter);
+				lblPropertyError.setVisible(true);
+				lblPropertyError.setText(Messages.EmptyFiledNotification);
+				return false;
+			}
+			propertyCounter++;
+		}
+		return true;
+	}
+
+	// Creates CellNAme Validator for table's cells
+	private ICellEditorValidator createNameEditorValidator(final String ErrorMessage) {
+		ICellEditorValidator propertyValidator = new ICellEditorValidator() {
+			@Override
+			public String isValid(Object value) {
+				String currentSelectedFld = table.getItem(table.getSelectionIndex()).getText();
+				String valueToValidate = String.valueOf(value).trim();
+				if (StringUtils.isEmpty(valueToValidate)) {
+					lblPropertyError.setText(ErrorMessage);
+					lblPropertyError.setVisible(true);
+					return ERROR;
+				}
+
+				for (RuntimeProperties temp : propertyList) {
+					if (!currentSelectedFld.equalsIgnoreCase(valueToValidate)
+							&& temp.getPropertyName().equalsIgnoreCase(valueToValidate)) {
+
+						lblPropertyError.setText(PROPERTY_EXISTS_ERROR);
+						lblPropertyError.setVisible(true);
+						return ERROR;
+					} else {
+						lblPropertyError.setVisible(false);
+					}
+				}
+				return null;
+			}
+		};
+		return propertyValidator;
+	}
+
+	// Creates Value Validator for table's cells
+	private ICellEditorValidator createValueEditorValidator(final String ErrorMessage) {
+		ICellEditorValidator propertyValidator = new ICellEditorValidator() {
+			@Override
+			public String isValid(Object value) {
+				table.getItem(table.getSelectionIndex()).getText();
+				String valueToValidate = String.valueOf(value).trim();
+				if (valueToValidate.isEmpty()) {
+					lblPropertyError.setText(ErrorMessage);
+					lblPropertyError.setVisible(true);
+					return ERROR; //$NON-NLS-1$
+				} else {
+					lblPropertyError.setVisible(false);
+				}
+				return null;
+
+			}
+		};
+		return propertyValidator;
+	}
+}
