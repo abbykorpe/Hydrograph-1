05e6cc0206c6dfe254e4aad609d295b895771d1c
Nitin Gupta <nitin.gupta@bitwiseglobal.com>
2016-03-23 14:15:29 +0530

    Added Java-doc and license info

--- com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/handler/GraphPropertiesHandler.java
+++ com.bitwise.app.graph/src/main/java/com/bitwise/app/graph/handler/GraphPropertiesHandler.java
@@ -1,3 +1,17 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+
 package com.bitwise.app.graph.handler;
 
 import org.apache.commons.lang.StringUtils;
@@ -13,6 +27,12 @@ import com.bitwise.app.graph.model.Container;
 import com.bitwise.app.graph.utility.SubGraphUtility;
 import com.bitwise.app.propertywindow.widgets.customwidgets.runtimeproperty.RuntimePropertyDialog;
 
+/**
+ * Handles the execution of graph level runtime properties. 
+ * 
+ * @author Bitwise
+ *
+ */
 public class GraphPropertiesHandler extends AbstractHandler implements IHandler {
 
 	@Override
