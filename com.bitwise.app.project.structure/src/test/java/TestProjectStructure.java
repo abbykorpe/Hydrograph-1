07a33c1c7de613e97d02cad3458c49c54a7a6501
rakesh kanchalwar <rakesh.kanchalwar@bitwiseglobal.com>
2016-03-28 17:38:30 +0530

    fix for project creation test case

--- com.bitwise.app.project.structure/src/test/java/TestProjectStructure.java
+++ com.bitwise.app.project.structure/src/test/java/TestProjectStructure.java
@@ -1,11 +1,26 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package test.java;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.JavaCore;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,7 +29,6 @@ import com.bitwise.app.project.structure.natures.ProjectNature;
 import com.bitwise.app.project.structure.wizard.ProjectStructureCreator;
  
  
-// TODO: Auto-generated Javadoc
 /**
  * The Class TestProjectStructure.
  * 
@@ -44,15 +58,16 @@ public class TestProjectStructure {
 	 * 
 	 * @throws CoreException
 	 *             the core exception
+	 * @throws IOException 
 	 */
     @SuppressWarnings("nls")
     @Test
-    public void testCreateProjectWithGoodArgs() throws CoreException {
+    public void testCreateProjectWithGoodArgs() throws CoreException, IOException {
         // This is the default workspace for this plug-in
         String workspaceFilePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
         String projectName = "delete-me";
         String projectPath = workspaceFilePath + File.separator + projectName;
- 
+        createFilesForTest();
         assertProjectDotFileAndStructureAndNatureExist(projectPath, projectName, null);
     }
  
@@ -83,12 +98,13 @@ public class TestProjectStructure {
     private void assertNatureIn(IProject project) throws CoreException {
         IProjectDescription descriptions = project.getDescription();
         String[] natureIds = descriptions.getNatureIds();
-        if (natureIds.length != 2) {
+        if (natureIds.length != 3) {
             Assert.fail("Not all natures found in project."); //$NON-NLS-1$
         }
  
         if (!natureIds[0].equals(ProjectNature.NATURE_ID) ||
-        		!natureIds[1].equals(JavaCore.NATURE_ID)) {
+        		!natureIds[1].equals(JavaCore.NATURE_ID) ||
+        		!natureIds[2].endsWith("org.eclipse.m2e.core.maven2Nature")){
             Assert.fail("Required project natures not found in project."); //$NON-NLS-1$
         }
     }
@@ -99,5 +115,23 @@ public class TestProjectStructure {
         if (!file.exists()) {
             Assert.fail("File " + projectFilePath + " does not exist."); //$NON-NLS-1$//$NON-NLS-2$
         }
+    }
+    
+    private void createFilesForTest() throws IOException{
+    	File configDir = new File(Platform.getInstallLocation().getURL().getPath() + "/config");
+    	if(!configDir.exists()){
+    		configDir.mkdir();
+    	}
+    	File mavenDir = new File(configDir.getPath() + "/maven");
+    	if(!mavenDir.exists()){
+    		mavenDir.mkdir();
+    	}
+    	File destinationFile = new File(mavenDir + "/pom.xml");
+    	if(destinationFile.exists()){
+    		return;
+    	}
+    	else{
+    		destinationFile.createNewFile();
+    	}
     }
 }
