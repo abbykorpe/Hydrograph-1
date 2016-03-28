6fe3af40581260bcdce76c9f9effcc7302f00678
Vibhor Tyagi <vibhor.tyagi@bitwiseglobal.com>
2016-03-28 15:08:08 +0530

    add java docs with license copy

--- com.bitwise.app.propertywindow/src/main/java/com/bitwise/app/propertywindow/runconfig/RunConfigDialog.java
+++ com.bitwise.app.propertywindow/src/main/java/com/bitwise/app/propertywindow/runconfig/RunConfigDialog.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+  * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  * http://www.apache.org/licenses/LICENSE2.0
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  ******************************************************************************/
+
 package com.bitwise.app.propertywindow.runconfig;
 
 import java.io.ByteArrayInputStream;
@@ -36,9 +49,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IFileEditorInput;
