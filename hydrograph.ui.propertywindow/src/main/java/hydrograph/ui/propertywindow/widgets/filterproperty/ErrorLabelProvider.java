c6c81a712fd8cfd1f9a90fefe87d37dd31a1833d
shashank Karnik <shashank.karnik@bitwiseglobal.com>
2016-04-19 14:25:58 +0530

    removed todo.

--- hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/filterproperty/ErrorLabelProvider.java
+++ hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/filterproperty/ErrorLabelProvider.java
@@ -29,49 +29,41 @@ public class ErrorLabelProvider implements ITableLabelProvider, ITableColorProvi
 
 	@Override
 	public void addListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public boolean isLabelProperty(Object element, String property) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void removeListener(ILabelProviderListener listener) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Font getFont(Object element, int columnIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Color getForeground(Object element, int columnIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Color getBackground(Object element, int columnIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Image getColumnImage(Object element, int columnIndex) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
