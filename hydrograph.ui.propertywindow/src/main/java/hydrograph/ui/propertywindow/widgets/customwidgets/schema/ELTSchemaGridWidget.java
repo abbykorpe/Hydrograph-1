fbec49fd524e0a5ffa84ca0af32089fa245f99b5
shashank Karnik <shashank.karnik@bitwiseglobal.com>
2016-04-19 14:09:50 +0530

    conflicts resolve.

--- hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/schema/ELTSchemaGridWidget.java
+++ hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/schema/ELTSchemaGridWidget.java
@@ -110,6 +110,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
