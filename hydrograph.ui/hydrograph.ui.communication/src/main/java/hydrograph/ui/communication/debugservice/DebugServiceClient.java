87db4921ab36fcef1f112cd6c2eb2e0dccb7fc35
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2016-06-12 19:26:05 +0530

    Fixed build issue

--- hydrograph.ui/hydrograph.ui.communication/src/main/java/hydrograph/ui/communication/debugservice/DebugServiceClient.java
+++ hydrograph.ui/hydrograph.ui.communication/src/main/java/hydrograph/ui/communication/debugservice/DebugServiceClient.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package hydrograph.ui.communication.debugservice;
 
 import hydrograph.ui.common.datastructures.dataviewer.JobDetails;
@@ -11,17 +24,46 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+/**
+ * 
+ * Debug service client
+ * 
+ * @author Bitwise
+ *
+ */
 public class DebugServiceClient {
 	public static DebugServiceClient INSTANCE = new DebugServiceClient();
 	
 	private DebugServiceClient() {		
 	}
 
+	/**
+	 * 
+	 * Method to delete debug file
+	 * 
+	 * @param jobDetails
+	 * @throws NumberFormatException
+	 * @throws HttpException
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public void deleteDebugFile(JobDetails jobDetails)
 			throws NumberFormatException, HttpException, MalformedURLException, IOException {
 		executePostMethod(Provider.INSTANCE.getDeleteDebugFileMethod(jobDetails));
 	}
 
+	/**
+	 * 
+	 * Method to get debug file
+	 * 
+	 * @param jobDetails
+	 * @param fileSize
+	 * @return debug file
+	 * @throws NumberFormatException
+	 * @throws HttpException
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public String getDebugFile(JobDetails jobDetails,String fileSize)
 			throws NumberFormatException, HttpException, MalformedURLException, IOException {
 		PostMethod method = Provider.INSTANCE.getDebugFileMethod(jobDetails,fileSize);
--- hydrograph.ui/hydrograph.ui.communication/src/main/java/hydrograph/ui/communication/debugservice/method/Provider.java
+++ hydrograph.ui/hydrograph.ui.communication/src/main/java/hydrograph/ui/communication/debugservice/method/Provider.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package hydrograph.ui.communication.debugservice.method;
 
 import java.net.MalformedURLException;
@@ -9,6 +22,13 @@ import hydrograph.ui.communication.debugservice.constants.DebugServicePostParame
 
 import org.apache.commons.httpclient.methods.PostMethod;
 
+/**
+ * 
+ * This class provides post methods for Debug service
+ * 
+ * @author Bitwise
+ *
+ */
 public class Provider {	
 	private static String POST_PROTOCOL="http";
 	public static Provider INSTANCE = new Provider();
@@ -17,6 +37,15 @@ public class Provider {
 		
 	}
 
+	/**
+	 * 
+	 * Get post method to delete csv debug file
+	 * 
+	 * @param jobDetails
+	 * @return {@link PostMethod}
+	 * @throws NumberFormatException
+	 * @throws MalformedURLException
+	 */
 	public PostMethod getDeleteDebugFileMethod(JobDetails jobDetails) throws NumberFormatException, MalformedURLException {
 		
 		URL url = new URL(POST_PROTOCOL, jobDetails.getHost(), Integer.valueOf(jobDetails.getPort()), DebugServiceMethods.DELETE_DEBUG_CSV_FILE);
@@ -29,6 +58,16 @@ public class Provider {
 		return postMethod;
 	}
 
+	/**
+	 * 
+	 * Get post method to get csv debug file
+	 * 
+	 * @param jobDetails
+	 * @param fileSize
+	 * @return {@link PostMethod}
+	 * @throws NumberFormatException
+	 * @throws MalformedURLException
+	 */
 	public PostMethod getDebugFileMethod(JobDetails jobDetails,String fileSize) throws NumberFormatException, MalformedURLException {
 		
 		URL url = new URL(POST_PROTOCOL, jobDetails.getHost(), Integer.valueOf(jobDetails.getPort()), DebugServiceMethods.GET_DEBUG_FILE_PATH);
