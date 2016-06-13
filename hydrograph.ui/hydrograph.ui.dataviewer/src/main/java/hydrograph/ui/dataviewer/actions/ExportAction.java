8c7f68232354f65f4b979e7113827b24487d626c
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-06-13 20:13:09 +0530

    ->Issues fix. ->Added cursor tooltip. ->Added warning icon on preference window.

--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
@@ -14,8 +14,10 @@
 package hydrograph.ui.dataviewer.actions;
 
 import hydrograph.ui.common.util.ConvertHexValues;
-import hydrograph.ui.dataviewer.datastructures.RowField;
+import hydrograph.ui.dataviewer.Activator;
+import hydrograph.ui.dataviewer.constants.Messages;
 import hydrograph.ui.dataviewer.datastructures.RowData;
+import hydrograph.ui.dataviewer.datastructures.RowField;
 import hydrograph.ui.dataviewer.preferencepage.ViewDataPreferences;
 import hydrograph.ui.dataviewer.window.DebugDataViewer;
 import hydrograph.ui.logging.factory.LogFactory;
@@ -54,13 +56,11 @@ public class ExportAction extends Action {
 	private String delimiter;
 	private String quoteCharactor;
 	private static final String EXPORT_FILE = "Export File";
-	private static final String PLUGIN_NAME = "hydrograph.ui.dataviewer";
 	private static final String EXPORT_DATA_DEFAULT_PATH = "exportDataDefaultpath";
 	private static final String DEFAULT = "default";
 	private static final String DEFAILT_FILE_NAME = "export_data.csv";
 	private static final String INFORMATION = "Information";
 	private static final String ERROR = "Error";
-	private String ERROR_MESSAGE = "File is open. Please close it to replace it.";
 	private Logger logger = LogFactory.INSTANCE.getLogger(ExportAction.class);
 	private static final String LABEL = "Export Data";
 
@@ -85,24 +85,17 @@ public class ExportAction extends Action {
 		addRowsDataInList(tableViewer, eachRowData, exportedfileDataList);
 		FileDialog fileDialog = new FileDialog(Display.getDefault().getActiveShell(), SWT.SAVE);
 		String filePath = getPathOfFileDialog(fileDialog);
-		try {
-			writeDataInFile(exportedfileDataList, filePath);
-		} catch (IOException e) {
-			logger.error("Error occur while writing data in csv File: " , e);
-		}
+		writeDataInFile(exportedfileDataList, filePath);
 	}
 
 	private void showMessage(String message, String messageBoxTitle, int icon) {
 		MessageBox messageBox = new MessageBox(new Shell(), SWT.OK | icon);
 		messageBox.setText(messageBoxTitle);
 		messageBox.setMessage(message);
-		int response = messageBox.open();
-		if (response == SWT.OK) {
-			// do nothing
-		}
+		messageBox.open();
 	}
 
-	private void writeDataInFile(List<String[]> fileDataList, String filePath) throws IOException {
+	private void writeDataInFile(List<String[]> fileDataList, String filePath){
 		CSVWriter writer = null;
 		FileWriter fileWriter = null;
 		try {
@@ -117,13 +110,21 @@ public class ExportAction extends Action {
 				}
 			}
 		} catch (IOException e1) {
-			showMessage(ERROR_MESSAGE, ERROR, SWT.ICON_ERROR);
+			showMessage(Messages.ERROR_MESSAGE, ERROR, SWT.ICON_ERROR);
 		} finally {
 			if (fileWriter != null) {
-				fileWriter.close();
+				try {
+					fileWriter.close();
+				} catch (IOException e) {
+					logger.error("Stream of File writer is not closing",e);
+				}
 			}
 			if (writer != null) {
-				writer.close();
+				try {
+					writer.close();
+				} catch (IOException e) {
+					logger.error("Stream of CSV writer is not closing",e);
+				}
 			}
 		}
 	}
@@ -141,8 +142,8 @@ public class ExportAction extends Action {
 	}
 
 	private String readExportDataDefaultPathFromFile() {
-		IScopeContext context = new InstanceScope();
-		IEclipsePreferences eclipsePreferences = context.getNode(PLUGIN_NAME);
+		IScopeContext context = InstanceScope.INSTANCE;
+		IEclipsePreferences eclipsePreferences = context.getNode(Activator.PLUGIN_ID);
 		String exportDataDefaultpath = eclipsePreferences.get(EXPORT_DATA_DEFAULT_PATH, DEFAULT);
 		exportDataDefaultpath = exportDataDefaultpath.equalsIgnoreCase(DEFAULT) ? " " : exportDataDefaultpath;
 		return exportDataDefaultpath;
--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/constants/Messages.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/constants/Messages.java
@@ -36,23 +36,30 @@ public class Messages extends NLS{
 	public static String QUOTE_VALUE_MATCH_ERROR;
 	public static String UNABLE_TO_RELOAD_DEBUG_FILE;
 	public static String UNABLE_TO_LOAD_DEBUG_FILE;
-	
 	public static String INVALID_DEBUG_FILE;
 	public static String END_OF_FILE;
 	public static String BEGINING_OF_FILE;
 	public static String ERROR_WHILE_FETCHING_RECORDS;
 	public static String JUMP_To_PAGE_OPERATION_NOT_ALLOWED;
-	
 	public static String JUMP_PAGE_TEXTBOX_CAN_NOTE_BE_EMPTY;
 	public static String FETCHING_PAGE;
 	public static String FETCHING_PREVIOUS_PAGE;
 	public static String FETCHING_NEXT_PAGE;
 	public static String SHOWING_RECORDS_FROM;
 	public static String ROW_COUNT;
-	
 	public static String FETCHING_TOTAL_NUMBER_OF_RECORDS;
-	
-	
+	public static String ERROR_MESSAGE;
+	public static String NUMERIC_VALUE_ACCPECTED ;
+	public static String FILE_SIZE_BLANK ;
+	public static String PAGE_SIZE_BLANK ;
+	public static String SINGLE_CHARACTOR_ERROR_MESSAGE ;
+	public static String DELIMITER_BLANK;
+	public static String QUOTE_CHARACTOR_BLANK;
+	public static String WARNING_MESSAGE;
+	public static String MEMORY_OVERFLOW_EXCEPTION;
+	public static String DUPLICATE_ERROR_MESSAGE;
+
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
