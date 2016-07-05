d214df260a3f418c4f58e91ec53501ed86aebcf6
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2016-07-05 17:36:43 +0530

    Added licences info

--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/AutoExpandColumns.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/AutoExpandColumns.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package hydrograph.ui.dataviewer.actions;
 
 import hydrograph.ui.dataviewer.window.DebugDataViewer;
@@ -6,6 +19,13 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.TableColumn;
 
+/**
+ * 
+ * Action to auto expand grid view columns
+ * 
+ * @author Bitwise
+ *
+ */
 public class AutoExpandColumns extends Action {
 	private static final String LABEL="Auto E&xapand Columns@Ctrl+X";
 	private DebugDataViewer debugDataViewer;
