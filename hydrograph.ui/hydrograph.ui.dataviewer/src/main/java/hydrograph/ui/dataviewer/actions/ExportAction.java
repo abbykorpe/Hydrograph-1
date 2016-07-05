f75d89e4c40ae74653aec983a45bb62c13bb56cc
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-07-05 14:52:41 +0530

    Refactoring

--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
@@ -114,7 +114,7 @@ public class ExportAction extends Action {
 	private String getPathOfFileDialog(FileDialog fileDialog) {
 		fileDialog.setText(EXPORT_FILE);
 		String exportDataDefaultpath = readExportDataDefaultPathFromFile();
-		if (exportDataDefaultpath.equalsIgnoreCase(" ")) {
+		if (StringUtils.isBlank(exportDataDefaultpath)) {
 			fileDialog.setFilterPath(null);
 		} else {
 			fileDialog.setFilterPath(exportDataDefaultpath);
