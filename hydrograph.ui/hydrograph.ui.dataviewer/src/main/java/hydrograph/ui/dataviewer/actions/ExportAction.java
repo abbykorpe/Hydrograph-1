31e7d11ce14a93f125e6d103bfcc76ba264200bf
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-07-05 13:17:28 +0530

    remove unwanted space

--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/actions/ExportAction.java
@@ -114,7 +114,6 @@ public class ExportAction extends Action {
 	private String getPathOfFileDialog(FileDialog fileDialog) {
 		fileDialog.setText(EXPORT_FILE);
 		String exportDataDefaultpath = readExportDataDefaultPathFromFile();
-		
 		if (exportDataDefaultpath.equalsIgnoreCase(" ")) {
 			fileDialog.setFilterPath(null);
 		} else {
