a5bac6d0e4de939bc6a5bade35e44e1958e9f32b
Sonia Raheja <sonia.raheja@bitwiseglobal.com>
2016-07-15 11:00:49 +0530

    remove unwanted code

--- hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/adapters/DataViewerAdapter.java
+++ hydrograph.ui/hydrograph.ui.dataviewer/src/main/java/hydrograph/ui/dataviewer/adapters/DataViewerAdapter.java
@@ -193,17 +193,6 @@ public class DataViewerAdapter {
 		results.close();
 	}
 
-//	public long getNoOfFilteredRows(){
-//		long rowCount = 0;
-//		String sql = new ViewDataQueryBuilder(tableName).column("COUNT(*)").getQuery(filterCondition);
-//		try (ResultSet rowCountResultSet = statement.executeQuery(sql)) {
-//			rowCountResultSet.next();
-//			rowCount = rowCountResultSet.getLong(1);
-//		} catch (SQLException sqlException) {
-//			logger.error("Error while counting the no of filtered rows", sqlException);
-//		}
-//		return rowCount;
-//	}
 
 	/**
 	 * 
