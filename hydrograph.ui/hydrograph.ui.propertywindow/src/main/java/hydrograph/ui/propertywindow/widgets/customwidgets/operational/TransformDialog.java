3b8b348ecfbdd57f00169c09e7728dff8e33d4d9
shashank Karnik <shashank.karnik@bitwiseglobal.com>
2016-08-01 19:32:10 +0530

    used stringUtils.

--- hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/operational/TransformDialog.java
+++ hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/operational/TransformDialog.java
@@ -1153,7 +1153,7 @@ public class TransformDialog extends Dialog implements IOperationClassDialog {
     	boolean isJobOld = false;
     	for(NameValueProperty nameValueProperty:transformMapping.getMapAndPassthroughField())
     	{
-    		if(nameValueProperty.getFilterProperty()==null || nameValueProperty.getFilterProperty().getPropertyname()==null)
+    		if(nameValueProperty.getFilterProperty()==null || StringUtils.isBlank(nameValueProperty.getFilterProperty().getPropertyname()))
     		{
     			isJobOld=true;
     			break;
