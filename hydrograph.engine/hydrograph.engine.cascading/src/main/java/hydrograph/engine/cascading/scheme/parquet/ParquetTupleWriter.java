ec554885dc9419a87eca9b6ace69169ce51e1044
gurdits <gurdit.singh@bitwiseglobal.com>
2016-08-16 12:48:09 +0530

    Refactored Avro and Parquet classes.

--- hydrograph.engine/hydrograph.engine.cascading/src/main/java/hydrograph/engine/cascading/scheme/parquet/ParquetTupleWriter.java
+++ hydrograph.engine/hydrograph.engine.cascading/src/main/java/hydrograph/engine/cascading/scheme/parquet/ParquetTupleWriter.java
@@ -1,21 +1,15 @@
-/* 
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+/*******************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package hydrograph.engine.cascading.scheme.parquet;
 
 import java.math.BigDecimal;
@@ -40,20 +34,13 @@ import parquet.schema.MessageTypeParser;
 import parquet.schema.Type;
 import cascading.tuple.TupleEntry;
 
-/**
- *
- *
- * @author Mickael Lacour <m.lacour@criteo.com>
- */
-public class TupleWriteSupport extends WriteSupport<TupleEntry> {
+
+public class ParquetTupleWriter extends WriteSupport<TupleEntry> {
 
 	private static final long MILLIS_PER_DAY = TimeUnit.DAYS.toMillis(1);
 	private static final Logger LOG = LoggerFactory
-			.getLogger(TupleWriteSupport.class);
+			.getLogger(ParquetTupleWriter.class);
 
-	// Local time zone.
-	// Java TimeZone has no mention of thread safety. Use thread local instance
-	// to be safe.
 	private static final ThreadLocal<TimeZone> LOCAL_TIMEZONE = new ThreadLocal<TimeZone>() {
 		@Override
 		protected TimeZone initialValue() {
