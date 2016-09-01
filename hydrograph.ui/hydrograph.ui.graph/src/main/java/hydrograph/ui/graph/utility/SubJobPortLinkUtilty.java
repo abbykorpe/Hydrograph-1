257053454b5ee7e7f9dfb4548dec9ec21fb33a75
rushikeshp <rushikeshp@GEBHSTC16.bitwiseglobal.net>
2016-09-01 19:55:45 +0530

    Fixed issue to Close Input/Output Subjob component properties window

--- hydrograph.ui/hydrograph.ui.graph/src/main/java/hydrograph/ui/graph/utility/SubJobPortLinkUtilty.java
+++ hydrograph.ui/hydrograph.ui.graph/src/main/java/hydrograph/ui/graph/utility/SubJobPortLinkUtilty.java
@@ -80,6 +80,7 @@ public class SubJobPortLinkUtilty {
 	   	if(cacheInputSubjobComp.size()>0){
 	   		inputSubComponent.getProperties().put(Constants.NAME, Constants.INPUT_SUBJOB);
 	   		inputSubComponent.setComponentLabel(Constants.INPUT_SUBJOB);
+	   		inputSubComponent.setType(Constants.INPUT_SUBJOB_TYPE);
 	   		inputSubComponent.setCategory(Constants.SUBJOB_COMPONENT_CATEGORY);
 	   		inputSubComponent.outputPortSettings(outPort);
 	   		fixComponentSize(inputSubComponent, outPort);
@@ -144,6 +145,7 @@ public class SubJobPortLinkUtilty {
 		   	if(cacheOutSubjobComp.size()>0){
 		   		outSubComponent.getProperties().put(Constants.NAME,Constants.OUTPUT_SUBJOB);
 		   		outSubComponent.setComponentLabel(Constants.OUTPUT_SUBJOB);
+		   		outSubComponent.setType(Constants.OUTPUT_SUBJOB_TYPE);
 		   		outSubComponent.setCategory(Constants.SUBJOB_COMPONENT_CATEGORY);
 		   		outSubComponent.inputPortSettings(inPort);	
 		   		fixComponentSize(outSubComponent, inPort);
