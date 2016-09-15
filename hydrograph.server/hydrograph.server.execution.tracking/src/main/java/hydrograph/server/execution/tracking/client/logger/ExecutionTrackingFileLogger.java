659243507e039adaee9949aa609fc5a81aa94863
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2016-09-15 17:08:23 +0530

    Replaced existing logger with sl4j Changed code to add unique records in log file updated code to have new log format

--- hydrograph.server/hydrograph.server.execution.tracking/src/main/java/hydrograph/server/execution/tracking/client/logger/ExecutionTrackingFileLogger.java
+++ hydrograph.server/hydrograph.server.execution.tracking/src/main/java/hydrograph/server/execution/tracking/client/logger/ExecutionTrackingFileLogger.java
@@ -12,30 +12,48 @@
  ******************************************************************************/
 package hydrograph.server.execution.tracking.client.logger;
 
-import java.io.BufferedWriter;
+import hydrograph.server.execution.tracking.server.status.datastructures.ComponentStatus;
+import hydrograph.server.execution.tracking.server.status.datastructures.ExecutionStatus;
+import hydrograph.server.execution.tracking.utils.ExecutionTrackingLogger;
+
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
 
-import hydrograph.server.execution.tracking.server.status.datastructures.ComponentStatus;
-import hydrograph.server.execution.tracking.server.status.datastructures.ExecutionStatus;
+class LastExecutionStatus{
+	private int statusNumber;
+	private ExecutionStatus executionStatus;
+	
+	public LastExecutionStatus(int statusNumber, ExecutionStatus executionStatus) {
+		super();
+		this.statusNumber = statusNumber;
+		this.executionStatus = executionStatus;
+	}
+
+	public int getStatusNumber() {
+		return statusNumber;
+	}
+
+	public ExecutionStatus getExecutionStatus() {
+		return executionStatus;
+	}
+	
+	@Override
+	public String toString() {
+		return "LastExecutionStatus [statusNumber=" + statusNumber + ", executionStatus=" + executionStatus + "]";
+	}	
+}
 
 /**
  * Use to manage logging
  * The Class ExecutionTrackingFileLogger.
  */
 public class ExecutionTrackingFileLogger {
-	
-	/** The Constant logger. */
-	private final static Logger logger = Logger.getLogger(ExecutionTrackingFileLogger.class);
-	
-	/** The Constant ExecutionTrackingLogFileExtention. */
-	private static final String ExecutionTrackingLogFileExtention = ".log";
 	
 	/** The Constant INSTANCE. */
 	public static final ExecutionTrackingFileLogger INSTANCE = new ExecutionTrackingFileLogger();
@@ -44,7 +62,16 @@ public class ExecutionTrackingFileLogger {
 	private String jobTrackingLogDirectory;
 		
 	/** The execution tracking loggers. */
-	private Map<String,BufferedWriter> executionTrackingLoggers;
+	private Map<String,LastExecutionStatus> lastExecutionStatusMap;
+	
+	
+	private static final String EXECUTION_STATUS_RECORD_SEPARATOR = " | ";
+	private static final String TIMESTAMP_FORMAT = "MM/dd/yyyy HH:mm:ss";
+	private static final String SUBMISSION_TIME = "Submission time: ";
+	private static final String JOB_ID = "Job ID: ";
+	
+	private static final String EXECUTION_TRACKING_LOG_FILE_EXTENTION = ".log";
+
 	
 	/**
 	 * Instantiates a new execution tracking file logger.
@@ -52,7 +79,7 @@ public class ExecutionTrackingFileLogger {
 	private ExecutionTrackingFileLogger(){
 		jobTrackingLogDirectory = "/tmp/JobTrackingLog/";
 				
-		executionTrackingLoggers = new HashMap();
+		lastExecutionStatusMap = new HashMap();
 		
 		createJobTrackingLogDirectory();
 	}
@@ -67,47 +94,73 @@ public class ExecutionTrackingFileLogger {
 		}
 	}
 	
+	private String getTimeStamp() {
+		String timeStamp = new SimpleDateFormat(TIMESTAMP_FORMAT).format(new Date());
+		return timeStamp;
+	}
+
+	
+	private String getHeader(ExecutionStatus executionStatus) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(JOB_ID);
+		stringBuilder.append(executionStatus.getJobId() + EXECUTION_STATUS_RECORD_SEPARATOR);
+		stringBuilder.append(SUBMISSION_TIME);
+		
+		String timeStamp = getTimeStamp();
+		stringBuilder.append(timeStamp + "\n");
+		return stringBuilder.toString();
+	}
+	
 	/**
-	 * Log.
-	 *
-	 * @param uniqJobId the uniq job id
-	 * @param executionStatus the execution status
+	 * 
+	 * Log the execution status
+	 * 
+	 * @param uniqJobId - unique job id
+	 * @param executionStatus - Execution status ({@link ExecutionStatus})
 	 */
 	public void log(String uniqJobId,ExecutionStatus executionStatus){
+		
+		String header=null;
+		if(lastExecutionStatusMap.get(uniqJobId)==null){
+			lastExecutionStatusMap.put(uniqJobId, new LastExecutionStatus(0, executionStatus));
+			header = getHeader(executionStatus);
+		}else{
+			header = null;
+			if(executionStatus.equals(lastExecutionStatusMap.get(uniqJobId).getExecutionStatus())){
+				return;
+			}			
+			lastExecutionStatusMap.put(uniqJobId, new LastExecutionStatus(lastExecutionStatusMap.get(uniqJobId).getStatusNumber() + 1, executionStatus));
+		}
+		
+		Logger executionTrackingLogger = getExecutionStatusLogger(uniqJobId);
+		
+		if(!StringUtils.isBlank(header)){
+			executionTrackingLogger.debug(header);
+		}
+		
 		String executionStatusString = getExecutionStatusInString(executionStatus);
 		if(StringUtils.isBlank(executionStatusString)){
 			return;
 		}
+		executionTrackingLogger.debug(executionStatusString);
 		
-		BufferedWriter executionTrackingLogger = getExecutionStatusLogger(uniqJobId);
-		
-		try {
-			executionTrackingLogger.write(executionStatusString);
-			executionTrackingLogger.flush();
-		} catch (IOException e) {
-			logger.info("Unable to write to execution tracking log file" + e.getMessage());
-		}
 	}
 
 	/**
 	 * Gets the execution status logger.
 	 *
-	 * @param uniqJobId the uniq job id
+	 * @param uniqJobId the unique job id
 	 * @return the execution status logger
 	 */
-	private BufferedWriter getExecutionStatusLogger(String uniqJobId) {
-		BufferedWriter bufferedWriter = executionTrackingLoggers.get(uniqJobId);		
-		if(bufferedWriter==null){
-			try{
-				 FileWriter fileWriter = new FileWriter(jobTrackingLogDirectory + uniqJobId + ExecutionTrackingLogFileExtention, true);
-				 bufferedWriter = new BufferedWriter(fileWriter);
-				 executionTrackingLoggers.put(uniqJobId, bufferedWriter);
-			}catch (Exception e) {
-				logger.info("Unable to create Execution Tracking Logger " + e.getMessage());
-			}
-		}
-		return bufferedWriter;
+	private Logger getExecutionStatusLogger(String uniqJobId) {	
+		
+		jobTrackingLogDirectory = jobTrackingLogDirectory + "//";
+		
+		 Logger logger = ExecutionTrackingLogger.INSTANCE.getLogger(uniqJobId, jobTrackingLogDirectory + uniqJobId + EXECUTION_TRACKING_LOG_FILE_EXTENTION);
+
+		return logger;
 	}
+	
 
 	/**
 	 * Gets the execution status in string.
@@ -120,18 +173,21 @@ public class ExecutionTrackingFileLogger {
 		if(executionStatus==null){
 			return null;
 		}
-		stringBuilder.append("Job ID " + executionStatus.getJobId() + "\n");
-		stringBuilder.append("Job Type: " + executionStatus.getType() + "\n");
-		
+
 		for(ComponentStatus componentStatus : executionStatus.getComponentStatus()){
-			stringBuilder.append("-------------------------------------\n");
-			stringBuilder.append("Component ID: " + componentStatus.getComponentId() + "\n");
-			stringBuilder.append("Current Status: " + componentStatus.getCurrentStatus() + "\n");
-			stringBuilder.append("Processed record count: " + componentStatus.getProcessedRecordCount().toString() + "\n");
+			
+			Map<String, Long> processCounts = componentStatus.getProcessedRecordCount();
+			
+			for(String portID: processCounts.keySet()){
+				stringBuilder.append(lastExecutionStatusMap.get(executionStatus.getJobId()).getStatusNumber() + EXECUTION_STATUS_RECORD_SEPARATOR);
+				stringBuilder.append(getTimeStamp() + EXECUTION_STATUS_RECORD_SEPARATOR);
+				stringBuilder.append(componentStatus.getComponentId() + EXECUTION_STATUS_RECORD_SEPARATOR);
+				stringBuilder.append(portID + EXECUTION_STATUS_RECORD_SEPARATOR);
+				stringBuilder.append(componentStatus.getCurrentStatus() + EXECUTION_STATUS_RECORD_SEPARATOR);
+				stringBuilder.append(processCounts.get(portID) + "\n");
+			}
 		}
-		
-		stringBuilder.append("============================================================================\n");
-		//logger.info(stringBuilder.toString());
+				
 		return stringBuilder.toString();
 	}
 
@@ -141,13 +197,6 @@ public class ExecutionTrackingFileLogger {
 	 * @param jobId the job id
 	 */
 	public void disposeLogger(String jobId){
-		if (executionTrackingLoggers.containsKey(jobId)) {
-			BufferedWriter bufferedWriter = executionTrackingLoggers.get(jobId);
-			try {
-				bufferedWriter.close();
-			} catch (IOException e) {
-				logger.info("Unable to close execution tracking logger" + e);
-			}
-		}
+		// TODO - needs to be implemented 
 	}
 }
