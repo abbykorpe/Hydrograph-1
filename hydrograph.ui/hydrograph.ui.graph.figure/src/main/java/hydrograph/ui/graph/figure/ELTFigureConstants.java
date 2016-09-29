c2f49a192151fccf52056a7e6052d02884f827c0
rushikeshp <rushikeshp@GEBHSTC16.bitwiseglobal.net>
2016-09-29 19:52:08 +0530

    Reduced size of compLabelOneLineLengthLimit field, so as to correctly display name of the 'Input Socket for Subjob' Component after Renaming.

--- hydrograph.ui/hydrograph.ui.graph.figure/src/main/java/hydrograph/ui/graph/figure/ELTFigureConstants.java
+++ hydrograph.ui/hydrograph.ui.graph.figure/src/main/java/hydrograph/ui/graph/figure/ELTFigureConstants.java
@@ -25,6 +25,6 @@ package hydrograph.ui.graph.figure;
 public class ELTFigureConstants {
 	public static final int componentOneLineLabelMargin = 16;
 	public static final int componentTwoLineLabelMargin = 32;
-	public static final int compLabelOneLineLengthLimit = 98;
+	public static final int compLabelOneLineLengthLimit = 93;
 	public static final String labelFont = "Arial";
 }
