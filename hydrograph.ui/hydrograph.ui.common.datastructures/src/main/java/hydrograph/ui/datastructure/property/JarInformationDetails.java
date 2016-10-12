a95a44a37cdc7aa17e465cb55127d17969f27634
ashikah <ashikah@GEBHSTC19.bitwiseglobal.net>
2016-10-12 18:57:46 +0530

    Installation Window Path Update for Libraries

--- hydrograph.ui/hydrograph.ui.common.datastructures/src/main/java/hydrograph/ui/datastructure/property/JarInformationDetails.java
+++ hydrograph.ui/hydrograph.ui.common.datastructures/src/main/java/hydrograph/ui/datastructure/property/JarInformationDetails.java
@@ -3,11 +3,12 @@ package hydrograph.ui.datastructure.property;
 public class JarInformationDetails {
 
 	private String name = "Jackson.jar";
-	private String versionNo="0.0.1.qualifier";
-	private String groupId="12345";
-	private String artifactNo="0001223";
-	private String licenseInfo="C:\\Users\\ashikah\\Git\\Hydrograph_Tool\\Thesis\\hydrograph.ui\\hydrograph.ui.help\\html\\Components\\Input_File_Delimited.html";
-	
+	private String versionNo = "0.0.1.qualifier";
+	private String groupId = "12345";
+	private String artifactNo = "0001223";
+	private String licenseInfo;
+	private String path;
+
 	public String getLicenseInfo() {
 		return licenseInfo;
 	}
@@ -19,49 +20,57 @@ public class JarInformationDetails {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
 	public String getVersionNo() {
 		return versionNo;
 	}
-	
+
 	public void setVersionNo(String versionNo) {
 		this.versionNo = versionNo;
 	}
-	
+
 	public String getGroupId() {
 		return groupId;
 	}
-	
+
 	public void setGroupId(String groupId) {
 		this.groupId = groupId;
 	}
-	
+
 	public String getArtifactNo() {
 		return artifactNo;
 	}
-	
+
 	public void setArtifactNo(String artifactNo) {
 		this.artifactNo = artifactNo;
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
-	      stringBuilder.append("JarInformation [ ");
-	      stringBuilder.append("\nName: ");
-	      stringBuilder.append(name);
-	      stringBuilder.append("\nVersion No: ");
-	      stringBuilder.append(versionNo);
-	      stringBuilder.append("\nGroup Id: ");
-	      stringBuilder.append(groupId);
-	      stringBuilder.append("\nArtifact No: ");
-	      stringBuilder.append(artifactNo);
-	      stringBuilder.append("\nLicense Info: ");
-	      stringBuilder.append(licenseInfo);
-	      stringBuilder.append(" ]");
-	      return stringBuilder.toString();
-	   }
+		stringBuilder.append("JarInformation [ ");
+		stringBuilder.append("\nName: ");
+		stringBuilder.append(name);
+		stringBuilder.append("\nVersion No: ");
+		stringBuilder.append(versionNo);
+		stringBuilder.append("\nGroup Id: ");
+		stringBuilder.append(groupId);
+		stringBuilder.append("\nArtifact No: ");
+		stringBuilder.append(artifactNo);
+		stringBuilder.append("\nLicense Info: ");
+		stringBuilder.append(licenseInfo);
+		stringBuilder.append(" ]");
+		return stringBuilder.toString();
+	}
 }
