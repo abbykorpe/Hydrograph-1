a239a99f9a9bbeb895890b481be0dc17c2303c7d
vibhort <vibhor-tyagi@bitwiseglobal.com>
2016-11-21 19:05:07 +0530

    Add license copy and java docs

--- hydrograph.ui/hydrograph.ui.graph/src/main/java/hydrograph/ui/graph/execution/tracking/replay/ViewExecutionHistoryDataDialog.java
+++ hydrograph.ui/hydrograph.ui.graph/src/main/java/hydrograph/ui/graph/execution/tracking/replay/ViewExecutionHistoryDataDialog.java
@@ -1,3 +1,15 @@
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package hydrograph.ui.graph.execution.tracking.replay;
 
 import java.io.FileNotFoundException;
@@ -38,6 +50,12 @@ import hydrograph.ui.graph.handler.ViewExecutionHistoryHandler;
 import hydrograph.ui.graph.job.Job;
 import hydrograph.ui.graph.job.JobManager;
 
+/**
+ * 
+ * The Class ViewExecutionHistoryDataDialog used to show execution tracking view history.
+ * @author Bitwise
+ *
+ */
 public class ViewExecutionHistoryDataDialog extends Dialog {
 	
 	private static final String VIEW_TRACKING_HISTORY = "View Execution Tracking History"; 
