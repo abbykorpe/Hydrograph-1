de735cc322d0720f219bb755a9787299d05a51f1
gurdits <gurdit.singh@bitwiseglobal.net>
2016-12-08 18:15:38 +0530

    Refactored DataBuilder class.

--- hydrograph.engine/hydrograph.engine.spark/src/test/scala/hydrograph/engine/spark/components/SparkUnionAllComponentTest.scala
+++ hydrograph.engine/hydrograph.engine.spark/src/test/scala/hydrograph/engine/spark/components/SparkUnionAllComponentTest.scala
@@ -3,9 +3,10 @@ package hydrograph.engine.spark.components
 import hydrograph.engine.core.component.entity.UnionAllEntity
 import hydrograph.engine.core.component.entity.elements.OutSocket
 import hydrograph.engine.spark.components.platform.BaseComponentParams
-import hydrograph.engine.testing.wrapper.{Bucket, DataBuilder}
+import hydrograph.engine.testing.wrapper.{Fields, Bucket, DataBuilder}
 import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 import org.junit.{Assert, Test}
+import java.lang.reflect.Type
 
 /**
   * Created by gurdits on 12/7/2016.
@@ -15,19 +16,17 @@ class SparkUnionAllComponentTest {
   @Test
   def TestSimpleUnionAllComponentWorking(): Unit ={
 
-
-    val schema = StructType(List(StructField("col1", DataTypes.StringType, true)
-      , StructField("col2", DataTypes.StringType, true)
-      , StructField("col3", DataTypes.StringType, true)
-      , StructField("col4", DataTypes.StringType, true)))
-
-    val df1= new DataBuilder(schema).addData(List("1", "C2R1", "C3Rx", "C4R1"))
+    val df1= new DataBuilder(Fields(List("col1","col2","col3","col4")).applyTypes(List(classOf[String],
+      classOf[String],classOf[String],classOf[String]))).addData(List
+    ("1",
+      "C2R1",
+      "C3Rx", "C4R1"))
       .addData(List("2", "C2R2", "C3Rx", "C4R2"))
       .addData(List("3", "C2R3", "C3Rx", "C4R3"))
       .build()
 
 
-    val df2= new DataBuilder(schema).addData(List("4", "C2R1", "C3Rx", "C4R1"))
+    val df2= new DataBuilder(Fields(List("col1","col2","col3","col4"))).addData(List("4", "C2R1", "C3Rx", "C4R1"))
       .addData(List("5", "C2R2", "C3Rx", "C4R2"))
       .addData(List("6", "C2R3", "C3Rx", "C4R3"))
       .build()
@@ -42,7 +41,7 @@ class SparkUnionAllComponentTest {
 
     val unionDF=new SparkUnionAllComponent(unionAllEntity,cp).createComponent()
 
-    val rows=Bucket(schema,unionDF.get("out1").get).result()
+    val rows=Bucket( Fields(List("col1","col2","col3","col4")),unionDF.get("out1").get).result()
 
     Assert.assertEquals(6,rows.size)
   }
