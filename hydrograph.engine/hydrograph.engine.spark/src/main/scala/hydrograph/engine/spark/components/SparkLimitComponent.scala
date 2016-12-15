96893d8e97676b3dd22c9db317b238eabeb90ed7
ankitsh <ankitsh@GEBHSTC11.bitwiseglobal.net>
2016-12-15 18:51:27 +0530

    Refactored code for Limit Component

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/SparkLimitComponent.scala
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/SparkLimitComponent.scala
@@ -7,34 +7,24 @@ import hydrograph.engine.core.component.entity.LimitEntity
 import hydrograph.engine.spark.components.base.StraightPullComponentBase
 import hydrograph.engine.spark.components.platform.BaseComponentParams
 
-/**
- * @author ankitsh
- *
- */
 class SparkLimitComponent(limitEntity : LimitEntity,componentsParams : BaseComponentParams) extends StraightPullComponentBase with Serializable{
 
 	val logger = LoggerFactory.getLogger(classOf[SparkLimitComponent])
 
-	/*
-	 * This method is used for creating limit component
-	 * with a given no. of records to be fetched in xml
-	 */
 	override def createComponent(): Map[String, DataFrame] = {
     try {
-      if (logger.isTraceEnabled()) {
-        logger.trace(limitEntity.toString());
-      }
+      logger.trace(limitEntity.toString());
       val output = componentsParams.getDataFrame().limit(limitEntity.getMaxRecord().toInt)
       val key = limitEntity.getOutSocketList.get(0).getSocketId()
-      
-      logger.trace("Creating limit assembly for '"
-        + limitEntity.getComponentId() + "' for socket: '"
-        + key + "' of type: '"
-        + limitEntity.getOutSocketList.get(0).getSocketType + "'");
-      
+
+      logger.info("Created limit component '"
+        + limitEntity.getComponentId() + "'with records: '" + limitEntity.getMaxRecord().toInt)
+      logger.debug("Executing limit component for socketId " + key + " of type: '" + limitEntity.getOutSocketList.get(0).getSocketType + "'");
+
       Map(key -> output)
+      
     } catch {
-      case e: RuntimeException => logger.error(e.getMessage, e); throw e
+      case e: RuntimeException => logger.error("Error in Limit Component : " + limitEntity.getComponentId() + "\n" + e.getMessage, e); throw e
     }
   }
 }
