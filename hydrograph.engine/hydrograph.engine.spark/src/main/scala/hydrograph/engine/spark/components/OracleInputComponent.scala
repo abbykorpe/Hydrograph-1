d23aa7fca9b651bf8992d9414c807b9d63c6b04b
Amiya Mishra <Amiya.Mishra@bitwiseglobal.com>
2016-12-20 17:24:38 +0530

    Added simple xml for input and output Oracle simple Added InputOracleAdapter and OutputOracleAdapter for oracle component

---
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/OracleInputComponent.scala
@@ -0,0 +1,56 @@
+package hydrograph.engine.spark.components
+
+import hydrograph.engine.core.component.entity.InputRDBMSEntity
+import hydrograph.engine.spark.components.base.InputComponentBase
+import hydrograph.engine.spark.components.platform.BaseComponentParams
+import hydrograph.engine.spark.components.utils.SchemaCreator
+import org.apache.spark.sql.DataFrame
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConverters._
+
+class OracleInputComponent(inputRDBMSEntity: InputRDBMSEntity, iComponentsParams: BaseComponentParams) extends
+  InputComponentBase {
+  val LOG: Logger = LoggerFactory.getLogger(classOf[OracleInputComponent])
+
+  override def createComponent(): Map[String, DataFrame] = {
+    val schemaField = SchemaCreator(inputRDBMSEntity).makeSchema()
+
+    val sparkSession = iComponentsParams.getSparkSession()
+    val prop = new java.util.Properties
+    prop.setProperty("user", inputRDBMSEntity.getUsername)
+    prop.setProperty("password", inputRDBMSEntity.getPassword)
+
+    LOG.info("Created Input Oracle Component '" + inputRDBMSEntity.getComponentId
+      + "' in Batch " + inputRDBMSEntity.getBatch
+      + " with output socket " + inputRDBMSEntity.getOutSocketList.get(0).getSocketId)
+
+    val tableorQuery = if (inputRDBMSEntity.getTableName == null || inputRDBMSEntity.getSelectQuery != null) ("(" + inputRDBMSEntity.getSelectQuery + ")") else inputRDBMSEntity.getTableName
+
+    if (inputRDBMSEntity.getTableName != null)
+      LOG.debug("Component Id '" + inputRDBMSEntity.getComponentId
+        + "' in Batch " + inputRDBMSEntity.getBatch
+        + " having schema: [ " + inputRDBMSEntity.getFieldsList.asScala.mkString(",") + " ]"
+        + " reading data from '" + tableorQuery + "' table")
+    else
+      LOG.debug("Component Id '" + inputRDBMSEntity.getComponentId
+        + "' in Batch " + inputRDBMSEntity.getBatch
+        + " having schema: [ " + inputRDBMSEntity.getFieldsList.asScala.mkString(",") + " ]"
+        + " reading data from '" + tableorQuery + "' query")
+
+    val connectionURL = "jdbc:oracle:" + inputRDBMSEntity.getDriverType + "://@" + inputRDBMSEntity.getHostName + ":" + inputRDBMSEntity.getPort() + "/" +
+      inputRDBMSEntity.getSid;
+
+    LOG.info("Connection  url for Oracle input component: " + connectionURL)
+
+    try {
+      val df = sparkSession.read.schema(schemaField).jdbc(connectionURL, tableorQuery, prop)
+      val key = inputRDBMSEntity.getOutSocketList.get(0).getSocketId
+      Map(key -> df)
+    } catch {
+      case e: Exception =>
+        LOG.error("Error in Input  Oracle input component '" + inputRDBMSEntity.getComponentId + "', Error" + e.getMessage, e)
+        throw new RuntimeException("Error in Input Oracle Component " + inputRDBMSEntity.getComponentId, e)
+    }
+  }
+}
---
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/OracleOutputComponent.scala
@@ -0,0 +1,71 @@
+package hydrograph.engine.spark.components
+
+import java.sql.SQLException
+
+import hydrograph.engine.core.component.entity.OutputRDBMSEntity
+import hydrograph.engine.spark.components.base.SparkFlow
+import hydrograph.engine.spark.components.platform.BaseComponentParams
+import hydrograph.engine.spark.components.utils.TableCreator
+import org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConverters._
+
+/**
+  * Created by amiyam on 16-12-2016.
+  */
+class OracleOutputComponent(outputRDBMSEntity: OutputRDBMSEntity, oComponentsParams: BaseComponentParams) extends
+  SparkFlow {
+  val LOG: Logger = LoggerFactory.getLogger(classOf[OracleOutputComponent])
+
+  override def execute(): Unit = {
+    val prop = new java.util.Properties
+    prop.setProperty("user", outputRDBMSEntity.getUsername)
+    prop.setProperty("password", outputRDBMSEntity.getPassword)
+
+    val connectionURL = "jdbc:oracle:" + outputRDBMSEntity.getDriverType + "://@" + outputRDBMSEntity.getHostName + ":" + outputRDBMSEntity.getPort() + "/" +
+      outputRDBMSEntity.getSid;
+
+    LOG.info("Created Output Oracle Component '" + outputRDBMSEntity.getComponentId
+      + "' in Batch " + outputRDBMSEntity.getBatch
+      + " with Connection url " + connectionURL
+      + " with data load option " + outputRDBMSEntity.getLoadType)
+    LOG.debug("Component Id '" + outputRDBMSEntity.getComponentId
+      + "' in Batch " + outputRDBMSEntity.getBatch
+      + " having schema [ " + outputRDBMSEntity.getFieldsList.asScala.mkString(",")
+      + " ] with load type " + outputRDBMSEntity.getLoadType
+      + " at connection url  " + connectionURL)
+
+    outputRDBMSEntity.getLoadType match {
+      case "newTable" =>
+        executeQuery(connectionURL, prop, TableCreator.getCreateTableQuery(outputRDBMSEntity))
+        oComponentsParams.getDataFrame().write.mode("append").jdbc(connectionURL, outputRDBMSEntity.getTableName, prop)
+      case "insert" => oComponentsParams.getDataFrame().write.mode("append").jdbc(connectionURL, outputRDBMSEntity.getTableName, prop)
+      case "truncateLoad" =>
+        executeQuery(connectionURL, prop, getTruncateQuery)
+        oComponentsParams.getDataFrame().write.mode("append").jdbc(connectionURL, outputRDBMSEntity.getTableName, prop)
+    }
+
+  }
+
+  def executeQuery(connectionURL: String, properties: java.util.Properties, query: String): Unit = {
+    LOG.debug("Executing '" + query + "' query for Oracle output component")
+    LOG.trace("In method executeQuery() executing '" + query + "' query with connection url " + connectionURL)
+    try {
+      val connection = JdbcUtils.createConnectionFactory(connectionURL, properties)()
+      val statment = connection.prepareStatement(query)
+      val resultSet = statment.executeUpdate()
+      connection.close()
+    } catch {
+      case e: SQLException =>
+        LOG.error("Error while connecting to database " + e.getMessage)
+        throw new RuntimeException("Error message " + e.getMessage, e)
+      case e: Exception =>
+        LOG.error("Error while executing '" + query + "' query in executeQuery()")
+        throw new RuntimeException("Error message " + e.getMessage, e)
+    }
+  }
+
+  def getTruncateQuery(): String = "truncate table " + outputRDBMSEntity.getTableName
+}
+
