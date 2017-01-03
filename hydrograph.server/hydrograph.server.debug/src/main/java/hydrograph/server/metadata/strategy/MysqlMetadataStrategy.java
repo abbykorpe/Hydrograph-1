fac2fb38b07c768b367cc0dd2d9498d0d5bcaf18
santlalg <santlal.gupta@bitwiseglobal.com>
2017-01-03 16:57:57 +0530

    removed Scale and precision from other datatype kept only for BigDecimal

--- hydrograph.server/hydrograph.server.debug/src/main/java/hydrograph/server/metadata/strategy/MysqlMetadataStrategy.java
+++ hydrograph.server/hydrograph.server.debug/src/main/java/hydrograph/server/metadata/strategy/MysqlMetadataStrategy.java
@@ -18,117 +18,113 @@ import java.util.Map;
  */
 public class MysqlMetadataStrategy extends MetadataStrategyTemplate {
 
-	Logger LOG = LoggerFactory.getLogger(MysqlMetadataStrategy.class);
-	Connection connection = null;
-	private String query = null, tableName = null;
+    Logger LOG = LoggerFactory.getLogger(MysqlMetadataStrategy.class);
+    Connection connection = null;
+    private String query = null, tableName = null;
 
-	/**
-	 * Used to set the connection for Mysql
-	 *
-	 * @param connectionProperties
-	 *            - contain request params details
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setConnection(Map connectionProperties) throws ClassNotFoundException, SQLException {
-		String host = connectionProperties
-				.getOrDefault(Constants.HOST_NAME,
-						new ParamsCannotBeNullOrEmpty(Constants.HOST_NAME + " not found in request parameter"))
-				.toString();
-		String port = connectionProperties
-				.getOrDefault(Constants.PORT_NUMBER,
-						new ParamsCannotBeNullOrEmpty(Constants.PORT_NUMBER + " not found in request parameter"))
-				.toString();
-		String userId = connectionProperties
-				.getOrDefault(Constants.USERNAME,
-						new ParamsCannotBeNullOrEmpty(Constants.USERNAME + " not found in request parameter"))
-				.toString();
-		String password = connectionProperties
-				.getOrDefault(Constants.PASSWORD,
-						new ParamsCannotBeNullOrEmpty(Constants.PASSWORD + " not found in request parameter"))
-				.toString();
-		String database = connectionProperties
-				.getOrDefault(Constants.DATABASE_NAME,
-						new ParamsCannotBeNullOrEmpty(Constants.DATABASE_NAME + " not found in request parameter"))
-				.toString();
-		String jdbcurl = "jdbc:mysql://" + host + ":" + port + "/" + database;
-		LOG.info("Connection url for mysql = '" + jdbcurl + "'");
-		LOG.info("Connecting with '" + userId + "' user id.");
-		Class.forName(Constants.MYSQL_JDBC_CLASSNAME);
-		connection = DriverManager.getConnection(jdbcurl, userId, password);
-	}
+    /**
+     * Used to set the connection for Mysql
+     *
+     * @param connectionProperties - contain request params details
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setConnection(Map connectionProperties) throws ClassNotFoundException, SQLException {
+        String host = connectionProperties
+                .getOrDefault(Constants.HOST_NAME,
+                        new ParamsCannotBeNullOrEmpty(Constants.HOST_NAME + " not found in request parameter"))
+                .toString();
+        String port = connectionProperties
+                .getOrDefault(Constants.PORT_NUMBER,
+                        new ParamsCannotBeNullOrEmpty(Constants.PORT_NUMBER + " not found in request parameter"))
+                .toString();
+        String userId = connectionProperties
+                .getOrDefault(Constants.USERNAME,
+                        new ParamsCannotBeNullOrEmpty(Constants.USERNAME + " not found in request parameter"))
+                .toString();
+        String password = connectionProperties
+                .getOrDefault(Constants.PASSWORD,
+                        new ParamsCannotBeNullOrEmpty(Constants.PASSWORD + " not found in request parameter"))
+                .toString();
+        String database = connectionProperties
+                .getOrDefault(Constants.DATABASE_NAME,
+                        new ParamsCannotBeNullOrEmpty(Constants.DATABASE_NAME + " not found in request parameter"))
+                .toString();
+        String jdbcurl = "jdbc:mysql://" + host + ":" + port + "/" + database;
+        LOG.info("Connection url for mysql = '" + jdbcurl + "'");
+        LOG.info("Connecting with '" + userId + "' user id.");
+        Class.forName(Constants.MYSQL_JDBC_CLASSNAME);
+        connection = DriverManager.getConnection(jdbcurl, userId, password);
+    }
 
-	/**
-	 * @param componentSchemaProperties
-	 *            - Contains request parameter details
-	 * @return {@link TableEntity}
-	 *
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	public TableEntity fillComponentSchema(Map componentSchemaProperties)
-			throws SQLException, ParamsCannotBeNullOrEmpty {
-		if (componentSchemaProperties.get(Constants.TABLENAME) != null)
-			tableName = componentSchemaProperties.get(Constants.TABLENAME).toString().trim();
-		else
-			query = componentSchemaProperties.get(Constants.QUERY).toString().trim();
+    /**
+     * @param componentSchemaProperties - Contains request parameter details
+     * @return {@link TableEntity}
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public TableEntity fillComponentSchema(Map componentSchemaProperties)
+            throws SQLException, ParamsCannotBeNullOrEmpty {
+        if (componentSchemaProperties.get(Constants.TABLENAME) != null)
+            tableName = componentSchemaProperties.get(Constants.TABLENAME).toString().trim();
+        else
+            query = componentSchemaProperties.get(Constants.QUERY).toString().trim();
 
-		LOG.info("Generating schema for mysql using "
-				+ ((tableName != null) ? "table : " + tableName : "query : " + query));
+        LOG.info("Generating schema for mysql using "
+                + ((tableName != null) ? "table : " + tableName : "query : " + query));
 
-		ResultSet res = null;
-		TableEntity tableEntity = new TableEntity();
-		List<TableSchemaFieldEntity> tableSchemaFieldEntities = new ArrayList<TableSchemaFieldEntity>();
-		try {
-			Statement stmt = connection.createStatement();
-			if (query != null && !query.isEmpty())
-				res = stmt.executeQuery("Select * from (" + query + ") as alias WHERE 1<0");
-			else if (tableName != null && !tableName.isEmpty())
-				res = stmt.executeQuery("Select * from " + tableName + " where 1<0");
-			else {
-				LOG.error("Table or query in request parameter cannot be null or empty " + Constants.QUERY + " => "
-						+ query + " " + Constants.TABLENAME + " => " + tableName);
-				throw new ParamsCannotBeNullOrEmpty("Table and query cannot be null or empty in request parameters: "
-						+ Constants.QUERY + " => " + query + " " + Constants.TABLENAME + " => " + tableName);
-			}
-			ResultSetMetaData rsmd = res.getMetaData();
-			for (int count = 1; count <= rsmd.getColumnCount(); count++) {
-				TableSchemaFieldEntity tableSchemaFieldEntity = new TableSchemaFieldEntity();
-				tableSchemaFieldEntity.setFieldName(rsmd.getColumnLabel(count));
+        ResultSet res = null;
+        TableEntity tableEntity = new TableEntity();
+        List<TableSchemaFieldEntity> tableSchemaFieldEntities = new ArrayList<TableSchemaFieldEntity>();
+        try {
+            Statement stmt = connection.createStatement();
+            if (query != null && !query.isEmpty())
+                res = stmt.executeQuery("Select * from (" + query + ") as alias WHERE 1<0");
+            else if (tableName != null && !tableName.isEmpty())
+                res = stmt.executeQuery("Select * from " + tableName + " where 1<0");
+            else {
+                LOG.error("Table or query in request parameter cannot be null or empty " + Constants.QUERY + " => "
+                        + query + " " + Constants.TABLENAME + " => " + tableName);
+                throw new ParamsCannotBeNullOrEmpty("Table and query cannot be null or empty in request parameters: "
+                        + Constants.QUERY + " => " + query + " " + Constants.TABLENAME + " => " + tableName);
+            }
+            ResultSetMetaData rsmd = res.getMetaData();
+            for (int count = 1; count <= rsmd.getColumnCount(); count++) {
+                TableSchemaFieldEntity tableSchemaFieldEntity = new TableSchemaFieldEntity();
+                tableSchemaFieldEntity.setFieldName(rsmd.getColumnLabel(count));
                 if (rsmd.getColumnTypeName(count).equalsIgnoreCase("TIMESTAMP")) {
-					tableSchemaFieldEntity.setFormat("yyyy-MM-dd HH:mm:ss");
-					tableSchemaFieldEntity.setFieldType("java.util.Date");
-				} else if(rsmd.getColumnTypeName(count).equalsIgnoreCase("DATE")) {
+                    tableSchemaFieldEntity.setFormat("yyyy-MM-dd HH:mm:ss");
+                    tableSchemaFieldEntity.setFieldType("java.util.Date");
+                } else if (rsmd.getColumnTypeName(count).equalsIgnoreCase("DATE")) {
                     tableSchemaFieldEntity.setFormat("yyyy-MM-dd");
                     tableSchemaFieldEntity.setFieldType("java.util.Date");
-                } else if(rsmd.getColumnTypeName(count).equalsIgnoreCase("TINYINT")) {
+                } else if (rsmd.getColumnTypeName(count).equalsIgnoreCase("TINYINT")) {
                     tableSchemaFieldEntity.setFieldType("java.lang.Boolean");
-				} else if(rsmd.getColumnTypeName(count).equalsIgnoreCase("SMALLINT"))  {
+                } else if (rsmd.getColumnTypeName(count).equalsIgnoreCase("SMALLINT")) {
                     tableSchemaFieldEntity.setFieldType("java.lang.Short");
-                } else if(rsmd.getColumnTypeName(count).equalsIgnoreCase("DECIMAL")){
+                } else if (rsmd.getColumnTypeName(count).equalsIgnoreCase("DECIMAL")) {
                     tableSchemaFieldEntity.setFieldType("java.math.BigDecimal");
                     tableSchemaFieldEntity.setScaleType("explicit");
-                }else{
+                    tableSchemaFieldEntity.setPrecision(String.valueOf(rsmd.getPrecision(count)));
+                    tableSchemaFieldEntity.setScale(String.valueOf(rsmd.getScale(count)));
+                } else {
                     tableSchemaFieldEntity.setFieldType(rsmd.getColumnClassName(count));
                 }
 
-
-				tableSchemaFieldEntity.setPrecision(String.valueOf(rsmd.getPrecision(count)));
-				tableSchemaFieldEntity.setScale(String.valueOf(rsmd.getScale(count)));
-				tableSchemaFieldEntities.add(tableSchemaFieldEntity);
-			}
-			if (componentSchemaProperties.get(Constants.TABLENAME) == null)
-				tableEntity.setQuery(componentSchemaProperties.get(Constants.QUERY).toString());
-			else
-				tableEntity.setTableName(componentSchemaProperties.get(Constants.TABLENAME).toString());
-			tableEntity.setDatabaseName(componentSchemaProperties.get(Constants.dbType).toString());
-			tableEntity.setSchemaFields(tableSchemaFieldEntities);
-			res.close();
-		} finally {
-			connection.close();
-		}
-		return tableEntity;
-	}
+                tableSchemaFieldEntities.add(tableSchemaFieldEntity);
+            }
+            if (componentSchemaProperties.get(Constants.TABLENAME) == null)
+                tableEntity.setQuery(componentSchemaProperties.get(Constants.QUERY).toString());
+            else
+                tableEntity.setTableName(componentSchemaProperties.get(Constants.TABLENAME).toString());
+            tableEntity.setDatabaseName(componentSchemaProperties.get(Constants.dbType).toString());
+            tableEntity.setSchemaFields(tableSchemaFieldEntities);
+            res.close();
+        } finally {
+            connection.close();
+        }
+        return tableEntity;
+    }
 }
