70d18d63999c12deac7e4c99f7ca5cc79f91fda0
Kalyan Rajpoot <Kalyan.Rajpoot@bitwiseglobal.com>
2017-01-06 18:19:22 +0530

    reading files from folder issue resolving

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/InputFileParquetComponent.scala
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/InputFileParquetComponent.scala
@@ -1,13 +1,12 @@
 package hydrograph.engine.spark.components
 
+import java.io.File
+
 import hydrograph.engine.core.component.entity.InputFileParquetEntity
 import hydrograph.engine.spark.components.base.InputComponentBase
 import hydrograph.engine.spark.components.platform.BaseComponentParams
 import hydrograph.engine.spark.components.utils.{SchemaCreator, SchemaUtils}
-import hydrograph.engine.spark.components.utils.ParquetMetadataReader
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.types.{StructField, StructType}
-
 import scala.collection.JavaConverters._
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -17,19 +16,35 @@ class InputFileParquetComponent(iFileParquetEntity: InputFileParquetEntity, iCom
 
   override def finalize(): Unit = super.finalize()
 
-
   override def createComponent(): Map[String, DataFrame] = {
     val schemaField = SchemaCreator(iFileParquetEntity).makeSchema()
     try {
 
+      val path: String = iFileParquetEntity.getPath
+
       val fieldList = iFileParquetEntity.getFieldsList.asScala
       fieldList.foreach { field => LOG.debug("Field name '" + field.getFieldName + "for Component " + iFileParquetEntity.getComponentId) }
 
-      val schemaFromMetadata: StructType = new ParquetMetadataReader().getParquetFileSchema(iFileParquetEntity.getPath, iComponentsParams.getSparkSession())
+      val listofFiles = getListOfFiles(path)
 
-      SchemaUtils().compareSchema(schemaField, schemaFromMetadata)
+      var df: DataFrame = null
 
-      val df = iComponentsParams.getSparkSession().read.schema(schemaField).parquet(iFileParquetEntity.getPath)
+      if (!listofFiles.isEmpty) {
+        for (i <- listofFiles.indices) {
+          df = iComponentsParams.getSparkSession().read.parquet(listofFiles(i).toString)
+          SchemaUtils().compareSchema(schemaField, df.schema)
+        }
+      }
+      else {
+        if (path.endsWith(".parquet")) {
+          df = iComponentsParams.getSparkSession().read.parquet(path)
+          SchemaUtils().compareSchema(schemaField, df.schema)
+        }
+        else {
+          throw new EmptyFolderException("There is no Parquet files in given specified Path: '" + path + "'")
+        }
+      }
+
       val key = iFileParquetEntity.getOutSocketList.get(0).getSocketId
 
       LOG.debug("Created Input File Parquet Component '" + iFileParquetEntity.getComponentId + "' in Batch" + iFileParquetEntity.getBatch
@@ -42,4 +57,21 @@ class InputFileParquetComponent(iFileParquetEntity: InputFileParquetEntity, iCom
         LOG.error("Error in Input  File Parquet component '" + iFileParquetEntity.getComponentId + "', Error" + ex.getMessage, ex); throw ex
     }
   }
+
+  /*
+   * get the list of files in folder
+   * @param dir String
+   * @return List[] of files
+   */
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
 }
+
+case class EmptyFolderException(message: String = "", cause: Throwable = null) extends Exception(message, cause)
