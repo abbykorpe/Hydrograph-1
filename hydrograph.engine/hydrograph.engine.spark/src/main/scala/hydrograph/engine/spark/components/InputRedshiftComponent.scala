145db9c3dd3c4509a11f1f4143dcb6e2b1d2b0c8
amiyam <Amiya.Mishra@bitwiseglobal.com>
2017-01-09 16:52:54 +0530

    Added redshift component specific parameters

---
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/InputRedshiftComponent.scala
@@ -0,0 +1,71 @@
+/** *****************************************************************************
+  * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  * http://www.apache.org/licenses/LICENSE-2.0
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  * *****************************************************************************/
+
+package hydrograph.engine.spark.components
+
+import hydrograph.engine.core.component.entity.InputRDBMSEntity
+import hydrograph.engine.spark.components.base.InputComponentBase
+import hydrograph.engine.spark.components.platform.BaseComponentParams
+import hydrograph.engine.spark.components.utils.{SchemaCreator, SchemaUtils}
+import org.apache.spark.sql.DataFrame
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConverters._
+
+class InputRedshiftComponent(inputRDBMSEntity: InputRDBMSEntity, iComponentsParams: BaseComponentParams) extends
+  InputComponentBase {
+  val LOG: Logger = LoggerFactory.getLogger(classOf[InputRedshiftComponent])
+
+  override def createComponent(): Map[String, DataFrame] = {
+    val schemaField = SchemaCreator(inputRDBMSEntity).makeSchema()
+
+    val sparkSession = iComponentsParams.getSparkSession()
+
+    val properties = inputRDBMSEntity.getRuntimeProperties;
+    properties.setProperty("user", inputRDBMSEntity.getUsername)
+    properties.setProperty("password", inputRDBMSEntity.getPassword)
+    properties.setProperty("driver", inputRDBMSEntity.getJdbcDriver)
+    LOG.info("Created Input Redshift Component '" + inputRDBMSEntity.getComponentId
+      + "' in Batch " + inputRDBMSEntity.getBatch
+      + " with output socket " + inputRDBMSEntity.getOutSocketList.get(0).getSocketId)
+
+    val tableorQuery = if (inputRDBMSEntity.getTableName == null || inputRDBMSEntity.getSelectQuery != null) ("(" + inputRDBMSEntity.getSelectQuery + ")") else inputRDBMSEntity.getTableName
+
+    if (inputRDBMSEntity.getTableName != null)
+      LOG.debug("Component Id '" + inputRDBMSEntity.getComponentId
+        + "' in Batch " + inputRDBMSEntity.getBatch
+        + " having schema: [ " + inputRDBMSEntity.getFieldsList.asScala.mkString(",") + " ]"
+        + " reading data from '" + tableorQuery + "' table")
+    else
+      LOG.debug("Component Id '" + inputRDBMSEntity.getComponentId
+        + "' in Batch " + inputRDBMSEntity.getBatch
+        + " having schema: [ " + inputRDBMSEntity.getFieldsList.asScala.mkString(",") + " ]"
+        + " reading data from '" + tableorQuery + "' query")
+
+    val connectionURL = "jdbc:redshift://" + inputRDBMSEntity.getHostName + ":" + inputRDBMSEntity.getPort() + "/" +
+      inputRDBMSEntity.getDatabaseName;
+
+    LOG.info("Connection  url for input Redshift  component: " + connectionURL)
+
+    try {
+      val df = sparkSession.read.jdbc(connectionURL, tableorQuery, properties)
+      SchemaUtils().compareSchema(schemaField, df.schema)
+      val key = inputRDBMSEntity.getOutSocketList.get(0).getSocketId
+      Map(key -> df)
+    } catch {
+      case e: Exception =>
+        LOG.error("Error in Input  Redshift component '" + inputRDBMSEntity.getComponentId + "', Error" + e.getMessage, e)
+        throw new RuntimeException("Error in Input Redshift Component " + inputRDBMSEntity.getComponentId, e)
+    }
+  }
+}
