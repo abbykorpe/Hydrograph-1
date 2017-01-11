20a41e5bc33753570a58f4ffd3335a45e4d32533
Amiya Mishra <amiya.mishra@bitwiseglobal.com>
2017-01-11 16:11:39 +0530

    Added jaxb classes

---
+++ hydrograph.engine/hydrograph.engine.core/src/main/java/hydrograph/engine/jaxb/oredshift/TypePrimaryKeys.java
@@ -0,0 +1,56 @@
+
+package hydrograph.engine.jaxb.oredshift;
+
+import hydrograph.engine.jaxb.commontypes.TypeKeyFields;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for type-primary-keys complex type.
+ * <p>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;complexType name="type-primary-keys">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="primaryKeys" type="{hydrograph/engine/jaxb/commontypes}type-key-fields" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "type-primary-keys", namespace = "hydrograph/engine/jaxb/oredshift", propOrder = {
+        "primaryKeys"
+})
+public class TypePrimaryKeys {
+
+    protected TypeKeyFields primaryKeys;
+
+    /**
+     * Gets the value of the primaryKeys property.
+     *
+     * @return possible object is
+     * {@link TypeKeyFields }
+     */
+    public TypeKeyFields getPrimaryKeys() {
+        return primaryKeys;
+    }
+
+    /**
+     * Sets the value of the primaryKeys property.
+     *
+     * @param value allowed object is
+     *              {@link TypeKeyFields }
+     */
+    public void setPrimaryKeys(TypeKeyFields value) {
+        this.primaryKeys = value;
+    }
+
+}
