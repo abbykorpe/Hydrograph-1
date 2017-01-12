60af1ef1e4d94cab51be1ce954288d9be6dc23ba
amiyam <Amiya.Mishra@bitwiseglobal.com>
2017-01-12 15:59:49 +0530

    Added default precision for bigdecimal type

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/utils/JavaToSQLTypeMapping.java
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/utils/JavaToSQLTypeMapping.java
@@ -120,9 +120,13 @@ public enum JavaToSQLTypeMapping {
         Map<String, String> map = selectMapping(databaseType).mapping();
         String[] arr = new String[fieldsDataType.length];
         for (int i = 0; i < fieldsDataType.length; i++) {
-            if (fieldsDataType[i].equals("java.math.BigDecimal"))
-                arr[i] = map.get(fieldsDataType[i]) + "(" + fieldsPrecision[i] + "," + fieldsScale[i] + ")";
-            else if (fieldsDataType[i].equals("java.util.Date") && fieldFormat[i].contains("HH:mm:ss"))
+            if (fieldsDataType[i].equals("java.math.BigDecimal")) {
+                if (fieldsPrecision[i] == -999) {
+                    fieldsPrecision[i] = 38;
+                    arr[i] = map.get(fieldsDataType[i]) + "(" + fieldsPrecision[i] + "," + fieldsScale[i] + ")";
+                } else
+                    arr[i] = map.get(fieldsDataType[i]) + "(" + fieldsPrecision[i] + "," + fieldsScale[i] + ")";
+            } else if (fieldsDataType[i].equals("java.util.Date") && fieldFormat[i].contains("HH:mm:ss"))
                 arr[i] = map.get("java.sql.Timestamp");
             else
                 arr[i] = map.get(fieldsDataType[i]);
