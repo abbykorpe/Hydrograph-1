5cdf9e9a125b2fde9c9d7eea66e5100228ccf4fe
Abhishek D Korpe <AbhishekK@bitwiseglobal.net>
2017-01-16 19:13:32 +0530

    Added support for reading nested XMLs

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/OutputFileXMLComponent.scala
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/OutputFileXMLComponent.scala
@@ -1,13 +1,12 @@
 package hydrograph.engine.spark.components
 
-import hydrograph.engine.core.component.entity.{OutputFileDelimitedEntity, OutputFileXMLEntity}
+import hydrograph.engine.core.component.entity.OutputFileXMLEntity
 import hydrograph.engine.spark.components.base.SparkFlow
 import hydrograph.engine.spark.components.platform.BaseComponentParams
 import hydrograph.engine.spark.components.utils.SchemaCreator
 import org.apache.spark.sql.{AnalysisException, SaveMode}
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
 /**
@@ -16,7 +15,7 @@ import scala.collection.JavaConverters._
 class OutputFileXMLComponent (outputFileXMLEntity: OutputFileXMLEntity, cp:
 BaseComponentParams) extends SparkFlow with Serializable {
 
-  private val LOG:Logger = LoggerFactory.getLogger(classOf[OutputFileDelimitedComponent])
+  private val LOG:Logger = LoggerFactory.getLogger(classOf[OutputFileXMLEntity])
 
   /* def createSchema(fields:util.List[SchemaField]): Array[Column] ={
      LOG.trace("In method createSchema()")
@@ -33,22 +32,29 @@ BaseComponentParams) extends SparkFlow with Serializable {
     //   val dateFormats=schemaCreator.getDateFormats()
     try {
       cp.getDataFrame().select(schemaCreator.createSchema():_*).write
+//        .option("charset", outputFileXMLEntity.getCharset)
+//        .option("strict", outputFileXMLEntity.isStrict)
+//        .option("dateFormats", schemaCreator.getDateFormats())
+//        .format("com.databricks.spark.xml")
         .option("charset", outputFileXMLEntity.getCharset)
-        .option("strict", outputFileXMLEntity.isStrict)
-        .option("dateFormats", schemaCreator.getDateFormats())
-        .format("hydrograph.engine.spark.datasource.delimited")
+        .option("rowTag", outputFileXMLEntity.getRowTag)
+        .option("rootTag", outputFileXMLEntity.getRootTag)
+        .mode( SaveMode.Overwrite)
+        .option("dateFormats", schemaCreator.getDateFormats)
+//        .schema(schemaCreator.makeSchema)
+        .format("com.databricks.spark.xml")
         .save(outputFileXMLEntity.getPath)
     } catch {
       case e: AnalysisException if (e.getMessage().matches("(.*)cannot resolve(.*)given input columns(.*)"))=>
-        LOG.error("Error in Output File Delimited Component "+ outputFileXMLEntity.getComponentId, e)
-        throw new RuntimeException("Error in Output File Delimited Component "
+        LOG.error("Error in Output File XML Component "+ outputFileXMLEntity.getComponentId, e)
+        throw new RuntimeException("Error in Output File XML Component "
           + outputFileXMLEntity.getComponentId, e )
       case e:Exception =>
-        LOG.error("Error in Output File Delimited Component "+ outputFileXMLEntity.getComponentId, e)
-        throw new RuntimeException("Error in Output File Delimited Component "
+        LOG.error("Error in Output File XML Component "+ outputFileXMLEntity.getComponentId, e)
+        throw new RuntimeException("Error in Output File XML Component "
           + outputFileXMLEntity.getComponentId, e)
     }
-    LOG.info("Created Output File Delimited Component "+ outputFileXMLEntity.getComponentId
+    LOG.info("Created Output File XML Component "+ outputFileXMLEntity.getComponentId
       + " in Batch "+ outputFileXMLEntity.getBatch +" with path " + outputFileXMLEntity.getPath)
     LOG.debug("Component Id: '"+ outputFileXMLEntity.getComponentId
       +"' in Batch: " + outputFileXMLEntity.getBatch
