81dbc5b0a11ca11573cc1de9a13b986497347caf
ashikah <ashikah@GEBHSTC19.bitwiseglobal.net>
2017-01-18 19:35:23 +0530

    Added TODO comment for Teradata FastLoad

--- hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/WidgetHelper.java
+++ hydrograph.ui/hydrograph.ui.propertywindow/src/main/java/hydrograph/ui/propertywindow/widgets/customwidgets/WidgetHelper.java
@@ -488,6 +488,7 @@ public class WidgetHelper {
 	
 	public WidgetConfig getSelectInterfaceWithLoadWidgetConfig() {
 		RadioButtonConfig radioConfig = new RadioButtonConfig();
+		//TODO Currently we don't have Fast_load utility at Engine side. So commenting the code.
 		radioConfig.setWidgetDisplayNames(new String []{Messages.STANDARD /*,Messages.FAST_LOAD*/});
 		//radioConfig.getRadioButtonListners().add(Listners.VERIFY_FAST_LOAD_FOR_TERADATA);
 		radioConfig.setPropertyName(Messages.LOAD_UTILITY_TYPE);
