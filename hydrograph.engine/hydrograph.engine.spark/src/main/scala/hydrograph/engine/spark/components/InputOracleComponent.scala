013cd9aa6cc40ad3b33ebdcf14fd1ef744783edc
Amiya Mishra <Amiya.Mishra@bitwiseglobal.com>
2017-01-23 19:19:14 +0530

    Changed logic for schema validation in input oracle type

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/InputOracleComponent.scala
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/InputOracleComponent.scala
@@ -3,8 +3,9 @@ package hydrograph.engine.spark.components
 import hydrograph.engine.core.component.entity.InputRDBMSEntity
 import hydrograph.engine.spark.components.base.InputComponentBase
 import hydrograph.engine.spark.components.platform.BaseComponentParams
-import hydrograph.engine.spark.components.utils.{SchemaCreator, SchemaUtils}
+import hydrograph.engine.spark.components.utils.{SchemaCreator, SchemaMismatchException}
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.JavaConverters._
@@ -46,7 +47,8 @@ class InputOracleComponent(inputRDBMSEntity: InputRDBMSEntity, iComponentsParams
 
     try {
       val df = sparkSession.read.jdbc(connectionURL, tableorQuery, properties)
-      SchemaUtils().compareSchema(schemaField, df.schema)
+
+      compareSchema(getSchema(schemaField), getMappedSchema(df.schema))
       val key = inputRDBMSEntity.getOutSocketList.get(0).getSocketId
       Map(key -> df)
     } catch {
@@ -54,5 +56,66 @@ class InputOracleComponent(inputRDBMSEntity: InputRDBMSEntity, iComponentsParams
         LOG.error("Error in Input  Oracle input component '" + inputRDBMSEntity.getComponentId + "', Error" + e.getMessage, e)
         throw new RuntimeException("Error in Input Oracle Component " + inputRDBMSEntity.getComponentId, e)
     }
+  }
+
+  def getSchema(schema: StructType): StructType = StructType(schema.toList.map(stuctField => new StructField(stuctField.name, getInputDataType(stuctField.dataType).getOrElse(stuctField.dataType))).toArray)
+
+  private def getInputDataType(dataType: DataType): Option[DataType] = {
+    val answer = dataType.typeName.toUpperCase match {
+      case "SHORT" => IntegerType
+      case "BOOLEAN" => StringType
+      case "DATE" => TimestampType
+      case _ => null
+    }
+    if (answer != null) Option(answer) else None
+
+  }
+
+  def getMappedSchema(schema: StructType): StructType = StructType(schema.toList.map(stuctField => new StructField(stuctField.name, getDataType(stuctField.dataType).getOrElse(stuctField.dataType))).toArray)
+
+  private def getDataType(dataType: DataType): Option[DataType] = {
+    val datatype = dataType.typeName.toUpperCase
+    val answer = getDataTypes(datatype)
+    if (answer != null) Option(answer) else None
+  }
+
+  def getDataTypes(datatype: String): DataType = {
+    if (datatype.matches("[(DECIMAL(]+[0-5],0[)]")) IntegerType else if (datatype.matches("[(DECIMAL(]+([6-9]|10),0[)]")) IntegerType else if (datatype.matches("[(DECIMAL(]+(1[1-9]),0[)]")) LongType else null
+  }
+
+  /*
+ * This will compare two schema and check whether @readSchema is exist in @mdSchema
+ * @param readSchema schema from input
+ * @param mdSchema MetaData schema from metadata
+ * @return Boolean true or false(Exception)
+ */
+  def compareSchema(readSchema: StructType, mdSchema: StructType): Boolean = {
+
+    val metaDataSchema = mdSchema.toList
+    val inputReadSchema = readSchema.toList
+
+    var dbDataType: DataType = null
+    var dbFieldName: String = null
+
+    inputReadSchema.foreach(f = inSchema => {
+      var fieldExist = metaDataSchema.exists(ds => {
+        dbDataType = ds.dataType
+        dbFieldName = ds.name
+        ds.name.equalsIgnoreCase(inSchema.name)
+      })
+      if (fieldExist) {
+        if (!(inSchema.dataType.typeName.equalsIgnoreCase("Float") || inSchema.dataType.typeName.equalsIgnoreCase("double")))
+          if (!(inSchema.dataType.typeName.equalsIgnoreCase(dbDataType.typeName))) {
+            LOG.error("Field '" + inSchema.name + "', data type does not match expected type:" + dbDataType + ", got type:" + inSchema.dataType)
+            throw SchemaMismatchException("Field '" + inSchema.name + "' data type does not match expected type:" + dbDataType + ", got type:" + inSchema.dataType)
+          }
+      }
+      else {
+        LOG.error("Field '" + inSchema.name + "' does not exist in metadata")
+        throw SchemaMismatchException("Input schema does not match with metadata schema, "
+          + "Field '" + inSchema.name + "' does not exist in metadata")
+      }
+    })
+    true
   }
 }
