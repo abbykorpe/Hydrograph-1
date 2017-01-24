87994dae7e69ec076867204a87f47abb00636cd6
Sandeep Vasande <Sandeep.Vasande@bitwiseglobal.com>
2017-01-24 16:18:05 +0530

    added runSql component

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/RunSQLComponentImpl.java
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/RunSQLComponentImpl.java
@@ -19,45 +19,36 @@ public class RunSQLComponentImpl implements RunSQLComponentInt {
         if (!properties.getProperty("databaseConnectionName").isEmpty()) {
             if (properties.getProperty("databaseConnectionName").equalsIgnoreCase("MYSQL")) {
                 log.debug("Request received test connection " + properties.getProperty("databaseConnectionName").equalsIgnoreCase("MYSQL"));
-                System.out.println("in mysql"+properties.getProperty("databaseConnectionName"));
                 try {
                     properties.setProperty("className", "com.mysql.jdbc.Driver");
                     conn = DriverManager.getConnection("jdbc:mysql://" + properties.getProperty("ip_address") + ":" + properties.getProperty("portNumber") + "/" + properties.getProperty("databaseName")+"?allowMultiQueries=true", properties.getProperty("userName"), properties.getProperty("password"));
                     conn.setAutoCommit(false);
                     PreparedStatement stmt = conn.prepareStatement(properties.getProperty("query"));
                     if ("select".toUpperCase().equals((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-                        System.out.println("in select");
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         ResultSet rs = stmt.executeQuery(properties.getProperty("query"));
                         while (rs.next()) {
                             conn.close();
                         }
                     } else if ("Insert".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Delete".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Truncate".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-                        System.out.println("in insert");
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         stmt.execute(properties.getProperty("query"));
                         conn.commit();
                         conn.close();
                     } else if ("Update".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Create".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Alter".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Drop".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-                        System.out.println("@@@@@@@@@@@@@@@@@@ In SP of MYSQL ");
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         stmt.executeUpdate(properties.getProperty("query"));
-                        System.out.println("@@@@@@@@@@@@@@@@@@ after SP of MYSQL ");
                         conn.commit();
-
                         conn.close();
                     }
                     else if ("Call".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Exec".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-                        System.out.println("in call");
                         CallableStatement callableStatement = conn.prepareCall(properties.getProperty("query"));
                         callableStatement.executeUpdate();
                         conn.commit();
-                        System.out.println("Query executed successfully.");
                         conn.close();
                     }
 
                 } catch (SQLException e) {
-                    System.out.println("in exception");
                     log.debug("SQL exception occurred ");
                     e.printStackTrace();
                 }
@@ -76,16 +67,13 @@ public class RunSQLComponentImpl implements RunSQLComponentInt {
                             conn.close();
                         }
                     } else if ("Insert".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Delete".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Truncate".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-
-                        System.out.println("In Query Insert");
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         stmt.execute(properties.getProperty("query"));
                         conn.commit();
                         System.out.println("Query executed successfully.");
                         conn.close();
                     } else if ("Update".toUpperCase().equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Create".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Alter".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase()) || "Drop".equalsIgnoreCase((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
-                        /*  System.out.println("In Query Update");
-                        log.debug("Request received test connection for execute " + properties.getProperty("query"));
+/*                        log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         stmt.executeUpdate(properties.getProperty("query"));
                         conn.commit();
                         System.out.println("Query executed successfully.");
@@ -95,7 +83,6 @@ public class RunSQLComponentImpl implements RunSQLComponentInt {
                         CallableStatement callableStatement = conn.prepareCall(properties.getProperty("query"));
                         callableStatement.executeUpdate();
                         conn.commit();
-                        System.out.println("Query executed successfully.");
                         conn.close();
                     }
 
@@ -110,7 +97,6 @@ public class RunSQLComponentImpl implements RunSQLComponentInt {
                     Class.forName("com.teradata.jdbc.TeraDriver");
                     conn = DriverManager.getConnection("jdbc:teradata://" + properties.getProperty("ip_address") + "/DATABASE=" + properties.getProperty("databaseName") + ",USER=" + properties.getProperty("userName") + ",PASSWORD=" + properties.getProperty("password"));
                     Statement stmt = conn.createStatement();
-//                    StringBuilder stringBuilder = new StringBuilder();
                     if ("select".toUpperCase().equals((properties.getProperty("query").split("\\s+"))[0].toUpperCase())) {
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         ResultSet rs = stmt.executeQuery(properties.getProperty("query"));
@@ -126,7 +112,6 @@ public class RunSQLComponentImpl implements RunSQLComponentInt {
                         log.debug("Request received test connection for execute " + properties.getProperty("query"));
                         stmt.executeUpdate(properties.getProperty("query"));
                         conn.commit();
-                        System.out.println("Query executed successfully.");
                         conn.close();
                     }
                 } catch (SQLException | ClassNotFoundException e) {
