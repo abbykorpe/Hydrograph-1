60ff4d60b1099c460b9161c1579233a1d28e5224
Sandeep Vasande <Sandeep.Vasande@bitwiseglobal.com>
2017-02-02 12:03:58 +0530

    Code refactoring for RunSQLComponent.

--- hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/RunSQLComponent.java
+++ hydrograph.engine/hydrograph.engine.spark/src/main/scala/hydrograph/engine/spark/components/RunSQLComponent.java
@@ -4,7 +4,6 @@ import com.ibatis.common.jdbc.ScriptRunner;
 import hydrograph.engine.core.component.entity.RunSqlEntity;
 import hydrograph.engine.core.component.entity.base.AssemblyEntityBase;
 import hydrograph.engine.spark.components.base.CommandComponentSparkFlow;
-import hydrograph.engine.spark.components.base.SparkFlow;
 import org.apache.log4j.Logger;
 
 import java.io.*;
@@ -32,104 +31,75 @@ public class RunSQLComponent extends CommandComponentSparkFlow implements Serial
     public void execute() {
         Properties properties = new Properties();
         Connection conn = null;
-        try {
 
-            if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("MYSQL")) {
-                log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
-                try {
-                    properties.setProperty("className", "com.mysql.jdbc.Driver");
-                    conn = DriverManager.getConnection("jdbc:mysql://" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName() + "?allowMultiQueries=true", runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
-                    conn.setAutoCommit(false);
-                    try {
-                        testScriptDemo(runSqlEntity.getQueryCommand());
-                        ScriptRunner sr = new ScriptRunner(conn, false, true);
-                        Reader reader = new BufferedReader(new FileReader(tempFile));
-                        sr.runScript(reader);
-                        reader.close();
-                        exitStatus_$eq(0);
-                    } catch (SQLException | IOException e) {
-                        exitStatus_$eq(-1);
-                        log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                } catch (Exception e) {
-                    exitStatus_$eq(-1);
-                    log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
-            } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Oracle")) {
-                log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
-                try {
-                    properties.setProperty("className", "oracle.jdbc.driver.OracleDriver");
-                    conn = DriverManager.getConnection("jdbc:oracle:thin://@" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName(), runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
-                    conn.setAutoCommit(false);
-                    try {
-                        testScriptDemo(runSqlEntity.getQueryCommand());
-                        ScriptRunner sr = new ScriptRunner(conn, false, true);
-                        Reader reader = new BufferedReader(new FileReader(tempFile));
-                        sr.runScript(reader);
-                        reader.close();
-                        exitStatus_$eq(0);
-                    } catch (Exception e) {
-                        exitStatus_$eq(-1);
-                        log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                } catch (SQLException e) {
-                    exitStatus_$eq(-1);
-                    log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
-            } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Teradata")) {
-                log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
-                try {
-                    properties.setProperty("className", "com.teradata.jdbc.TeraDriver");
-                    conn = DriverManager.getConnection("jdbc:teradata://" + runSqlEntity.getServerName() + "/DATABASE=" + runSqlEntity.getDatabaseName() + ",USER=" + runSqlEntity.getDbUserName() + ",PASSWORD=" + runSqlEntity.getDbPassword());
-                    conn.setAutoCommit(false);
-                    try {
-                        testScriptDemo(runSqlEntity.getQueryCommand());
-                        ScriptRunner sr = new ScriptRunner(conn, false, true);
-                        Reader reader = new BufferedReader(new FileReader(tempFile));
-                        sr.runScript(reader);
-                        reader.close();
-                        exitStatus_$eq(0);
-                    } catch (Exception e) {
-                        exitStatus_$eq(-1);
-                        log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                } catch (SQLException e) {
-                    exitStatus_$eq(-1);
-                    log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
-            } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Redshift")) {
-                log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
-                try {
-                    properties.setProperty("className", "com.amazon.redshift.jdbc42.Driver");
-                    conn = DriverManager.getConnection("jdbc:redshift://" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName(), runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
-                    conn.setAutoCommit(false);
-                    try {
-                        testScriptDemo(runSqlEntity.getQueryCommand());
-                        ScriptRunner sr = new ScriptRunner(conn, false, true);
-                        Reader reader = new BufferedReader(new FileReader(tempFile));
-                        sr.runScript(reader);
-                        reader.close();
-                        exitStatus_$eq(0);
-                    } catch (Exception e) {
-                        exitStatus_$eq(-1);
-                        log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                } catch (SQLException e) {
-                    exitStatus_$eq(-1);
-                    log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
-                    throw new RuntimeException(e);
-                }
+        if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("MYSQL")) {
+            log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
+            try {
+                properties.setProperty("className", "com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName() + "?allowMultiQueries=true", runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
+                conn.setAutoCommit(false);
+                testScriptDemo(runSqlEntity.getQueryCommand());
+                ScriptRunner sr = new ScriptRunner(conn, false, true);
+                Reader reader = new BufferedReader(new FileReader(tempFile));
+                sr.runScript(reader);
+                reader.close();
+                exitStatus_$eq(0);
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                exitStatus_$eq(-1);
+                log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            exitStatus_$eq(-1);
-            e.printStackTrace();
+        } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Oracle")) {
+            log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
+            try {
+                properties.setProperty("className", "oracle.jdbc.driver.OracleDriver");
+                conn = DriverManager.getConnection("jdbc:oracle:thin://@" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName(), runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
+                conn.setAutoCommit(false);
+                testScriptDemo(runSqlEntity.getQueryCommand());
+                ScriptRunner sr = new ScriptRunner(conn, false, true);
+                Reader reader = new BufferedReader(new FileReader(tempFile));
+                sr.runScript(reader);
+                reader.close();
+                exitStatus_$eq(0);
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                exitStatus_$eq(-1);
+                log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Teradata")) {
+            log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
+            try {
+                properties.setProperty("className", "com.teradata.jdbc.TeraDriver");
+                conn = DriverManager.getConnection("jdbc:teradata://" + runSqlEntity.getServerName() + "/DATABASE=" + runSqlEntity.getDatabaseName() + ",USER=" + runSqlEntity.getDbUserName() + ",PASSWORD=" + runSqlEntity.getDbPassword());
+                conn.setAutoCommit(false);
+                testScriptDemo(runSqlEntity.getQueryCommand());
+                ScriptRunner sr = new ScriptRunner(conn, false, true);
+                Reader reader = new BufferedReader(new FileReader(tempFile));
+                sr.runScript(reader);
+                reader.close();
+                exitStatus_$eq(0);
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                exitStatus_$eq(-1);
+                log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+        } else if (runSqlEntity.getDatabaseConnectionName().equalsIgnoreCase("Redshift")) {
+            log.debug("Request received test connection " + runSqlEntity.getDatabaseConnectionName());
+            try {
+                properties.setProperty("className", "com.amazon.redshift.jdbc42.Driver");
+                conn = DriverManager.getConnection("jdbc:redshift://" + runSqlEntity.getServerName() + ":" + runSqlEntity.getPortNumber() + "/" + runSqlEntity.getDatabaseName(), runSqlEntity.getDbUserName(), runSqlEntity.getDbPassword());
+                conn.setAutoCommit(false);
+                testScriptDemo(runSqlEntity.getQueryCommand());
+                ScriptRunner sr = new ScriptRunner(conn, false, true);
+                Reader reader = new BufferedReader(new FileReader(tempFile));
+                sr.runScript(reader);
+                reader.close();
+                exitStatus_$eq(0);
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                exitStatus_$eq(-1);
+                log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -141,7 +111,7 @@ public class RunSQLComponent extends CommandComponentSparkFlow implements Serial
             bufferedWriter.write(statement);
         } catch (IOException e) {
             exitStatus_$eq(-1);
-            e.printStackTrace();
+            log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + e.getMessage());
         } finally {
             try {
                 if (bufferedWriter != null)
@@ -150,13 +120,14 @@ public class RunSQLComponent extends CommandComponentSparkFlow implements Serial
                     fileWriter.close();
             } catch (IOException ex) {
                 exitStatus_$eq(-1);
-                ex.printStackTrace();
+                log.debug("Failed to Execute" + runSqlEntity.getQueryCommand() + " The error is " + ex.getMessage());
             }
         }
     }
 
     @Override
     public int exitStatus() {
+
         return super.exitStatus();
     }
 
@@ -165,4 +136,5 @@ public class RunSQLComponent extends CommandComponentSparkFlow implements Serial
         super.exitStatus_$eq(exitStatus);
     }
 }
+
 
