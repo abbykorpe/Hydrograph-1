6394dfbcbd6a150f1309f811c7dc70ed9857f503
Shrirang S Kumbhar <Shrirang.Kumbhar@bitwiseglobal.com>
2017-02-06 16:57:47 +0530

    Enhancement:
    1. Updated code to have gradle nature to hydrograph project
    2. Updated Mac OS launcher icon

--- hydrograph.ui/hydrograph.ui.project.structure/src/main/java/hydrograph/ui/project/structure/wizard/ProjectStructureCreator.java
+++ hydrograph.ui/hydrograph.ui.project.structure/src/main/java/hydrograph/ui/project/structure/wizard/ProjectStructureCreator.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright 2017 Capital One Services, LLC and Bitwise, Inc.
+/********************************************************************************
+ * Copyright 2016 Capital One Services, LLC and Bitwise, Inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ ******************************************************************************/
 
  
 package hydrograph.ui.project.structure.wizard;
@@ -17,12 +17,7 @@ package hydrograph.ui.project.structure.wizard;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.URI;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,7 +46,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.slf4j.Logger;
-
 import hydrograph.ui.common.util.Constants;
 import hydrograph.ui.logging.factory.LogFactory;
 import hydrograph.ui.project.structure.Activator;
@@ -64,10 +58,12 @@ import hydrograph.ui.project.structure.natures.ProjectNature;
  */
 public class ProjectStructureCreator {
 
-	private static final String ORG_ECLIPSE_M2E_CORE_MAVEN2_NATURE = "org.eclipse.m2e.core.maven2Nature";
-	private static final String POM_XML = "pom.xml";
-	private static final String MAVEN = "maven";
-	private static final String TEMPLATE_PROJECT_NAME = "templateProjectName";
+	private static final String JAVA_PROJECT_BUILDER = "org.eclipse.jdt.core.javabuilder";
+	private static final String HYDROGRAPH_PROJECT_BUILDER = "org.eclipse.buildship.core.gradleprojectbuilder";
+	private static final String BUILD_TOOL_CONTANER = "org.eclipse.buildship.core.gradleclasspathcontainer";
+	private static final String HYDROGRAPH_PROJECT_SETTINGS_DIRECTORY_NAME = ".settings";
+	private static final String GRADLE_SETTINGS_DIRECTORY = "gradlesettings";
+	private static final String GRADLE_NATURE = "org.eclipse.buildship.core.gradleprojectnature";
 	private static final String PROPERTIES = "properties";
 	private static final String BUILD = "build";
 
@@ -116,24 +112,16 @@ public class ProjectStructureCreator {
 
 				List<IClasspathEntry> entries = addJavaLibrariesInClassPath();
 
-				IFolder libFolder = project.getFolder(CustomMessages.ProjectSupport_LIB);
-
-				//add libs to project class path 
 				String installLocation = Platform.getInstallLocation().getURL().getPath();
 
-				//copyExternalLibAndAddToClassPath(installLocation + CustomMessages.ProjectSupport_LIB, libFolder, entries);
-
 				copyBuildFile(installLocation + CustomMessages.ProjectSupport_CONFIG_FOLDER + "/" + 
-						CustomMessages.ProjectSupport_GRADLE + "/" + BUILD, project);
+						CustomMessages.ProjectSupport_GRADLE + "/" + BUILD, project,false);
 				
 				copyBuildFile(installLocation + CustomMessages.ProjectSupport_CONFIG_FOLDER + "/" + 
-						CustomMessages.ProjectSupport_GRADLE + "/" + PROPERTIES, project);
+						CustomMessages.ProjectSupport_GRADLE + "/" + PROPERTIES, project,false);
 				
-				copyBuildFile(installLocation + CustomMessages.ProjectSupport_CONFIG_FOLDER + "/" + 
-						MAVEN, project);
-				
-				updateMavenFile(POM_XML, project);
-				
+				copyBuildFile(installLocation + CustomMessages.ProjectSupport_CONFIG_FOLDER + "/" + GRADLE_SETTINGS_DIRECTORY, project,true);
+					
 				javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
 
 				//set source folder entry in classpath
@@ -148,12 +136,12 @@ public class ProjectStructureCreator {
 		return project;
 	} 
 
-	private void copyBuildFile(String source, IProject project) throws CoreException {
+	private void copyBuildFile(String source, IProject project, boolean settingFile) throws CoreException {
 		File sourceFileLocation = new File(source);
 		File[] listFiles = sourceFileLocation.listFiles();
 		if(listFiles != null){
 			for(File sourceFile : listFiles){
-				IFile destinationFile = project.getFile(sourceFile.getName());
+				IFile destinationFile = getDestinationFile(project, settingFile, sourceFile);
 				try {
 					if(!destinationFile.exists()){ //used while importing a project
 						destinationFile.create(new FileInputStream(sourceFile), true, null);
@@ -166,20 +154,17 @@ public class ProjectStructureCreator {
 		}
 	}
 
-	private void updateMavenFile(String source, IProject project) throws CoreException {
-		try{
-			IFile destinationFile = project.getFile(source);
-			java.nio.file.Path path = Paths.get(destinationFile.getLocationURI());
-			Charset charset = StandardCharsets.UTF_8;
-			String content = new String(Files.readAllBytes(path), charset);
-			content = content.replaceAll(TEMPLATE_PROJECT_NAME, project.getName());
-			Files.write(path, content.getBytes(charset));
-		} catch (IOException exception) {
-			logger.debug("Could not change the group and artifact name");
-			throw new CoreException(new MultiStatus(Activator.PLUGIN_ID, 100, "Could not change the group and artifact name", exception));
+	private IFile getDestinationFile(IProject project, boolean settingFile, File sourceFile) {
+		IFile destinationFile= null;
+		if(!settingFile){
+			destinationFile= project.getFile(sourceFile.getName());
+		}else{
+			destinationFile = project.getFile(HYDROGRAPH_PROJECT_SETTINGS_DIRECTORY_NAME + "/" + sourceFile.getName());
 		}
+		return destinationFile;
 	}
-	
+
+		
 	/**
 	 * Sets the <b>src</b> folder as the source folder in project
 	 * @param project
@@ -204,7 +189,7 @@ public class ProjectStructureCreator {
 	private List<IClasspathEntry> addJavaLibrariesInClassPath() {
 		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
 		entries.add(JavaCore.newContainerEntry(JavaRuntime.newDefaultJREContainerPath()));
-		entries.add(JavaCore.newContainerEntry(new Path("org.eclipse.m2e.MAVEN2_CLASSPATH_CONTAINER")));
+		entries.add(JavaCore.newContainerEntry(new Path(BUILD_TOOL_CONTANER)));
 		return entries;
 	}
 
@@ -244,16 +229,16 @@ public class ProjectStructureCreator {
 			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
 			newNatures[prevNatures.length] = ProjectNature.NATURE_ID;
 			newNatures[prevNatures.length + 1] = JavaCore.NATURE_ID;
-			newNatures[prevNatures.length + 2] = ORG_ECLIPSE_M2E_CORE_MAVEN2_NATURE;
+			newNatures[prevNatures.length + 2] = GRADLE_NATURE;
 
 			// validate the natures
 			IWorkspace workspace = ResourcesPlugin.getWorkspace();
 			IStatus status = workspace.validateNatureSet(newNatures); 
 			ICommand javaBuildCommand= description.newCommand();
-			javaBuildCommand.setBuilderName("org.eclipse.jdt.core.javabuilder");
-			ICommand mavenBuildCommand= description.newCommand();
-			mavenBuildCommand.setBuilderName("org.eclipse.m2e.core.maven2Builder");
-			ICommand[] iCommand = {javaBuildCommand, mavenBuildCommand};
+			javaBuildCommand.setBuilderName(JAVA_PROJECT_BUILDER);
+			ICommand hydrographProjectBuildCommand= description.newCommand();
+			hydrographProjectBuildCommand.setBuilderName(HYDROGRAPH_PROJECT_BUILDER);
+			ICommand[] iCommand = {javaBuildCommand, hydrographProjectBuildCommand};
 			description.setBuildSpec(iCommand); 
 			// only apply new nature, if the status is ok
 			if (status.getCode() == IStatus.OK) {
@@ -316,31 +301,7 @@ public class ProjectStructureCreator {
 		return newProject;
 	}
 
-	/*
-	 * Copy External jar to project lib directory
-	 * @param source path
-	 * @param target path
-	 * @return list of added files path
-	 */
-	private void copyExternalLibAndAddToClassPath(String source,IFolder destinationFolder, List<IClasspathEntry> entries) throws CoreException{
-		File sourceFileLocation = new File(source);
-		File[] listFiles = sourceFileLocation.listFiles();
-		if(listFiles != null){
-			for(File sourceFile : listFiles){
-				IFile destinationFile = destinationFolder.getFile(sourceFile.getName());
-				try {
-					if(!destinationFile.exists()){ //used while importing a project
-						destinationFile.create(new FileInputStream(sourceFile), true, null);
-					}
-					entries.add(JavaCore.newLibraryEntry(new Path(destinationFile.getLocation().toOSString()), null, null));
-				} catch (FileNotFoundException | CoreException exception) {
-					logger.debug("Copy external library files operation failed", exception);
-					throw new CoreException(new MultiStatus(Activator.PLUGIN_ID, 101, "Copy external library files operation failed", exception));
-				}
-			}
-		}
-	}
-
+	
 	/**
 	 * The Class InvalidProjectNameException.
 	 * 
